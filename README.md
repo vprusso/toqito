4d4435620872585e3ec7fcefbb135f6f8975c0d2
Vincent Russo <vincentrusso1@gmail.com>
2020-01-21 23:14:38 -0500

    Adding setup and travis config files

---
+++ README.md
@@ -0,0 +1,12 @@
+# ToQITo (Theory of Quantum Information Toolkit)
+
+Suite of Python tools that come in handy when considering various concepts in quantum information.
+ 
+## Testing
+
+The `nose` module is used for testing. To run the suite of tests for `toqito`, run the following command in the 
+root directory of this project.
+
+    nosetests --with-coverage --cover-erase --cover-package .
+
+
---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+setup(
+    name="toqito",
+    description="Python module for quantum information."
+    long_description=open("README.md").read(),
+    url="https://github.com/vprusso/toqito",
+
+    author="Vincent Russo",
+    author_email="vincentrusso1@gmail.com",
+
+    packages=["toqito"],
+    version="0.1.0",
+
+    license="MIT",
+
+    keywords=["toqito", "quantum information", "quantum computing"],
+
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.5",
+    ],
+
+    project_urls={
+        "Homepage": "http://vprusso.github.io/",
+    }
+)
