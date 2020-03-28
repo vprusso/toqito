3e8dfef04dee7b8509456e5230f84a88170af461
Vincent Russo <vincentrusso1@gmail.com>
2020-03-28 19:11:01 -0400

    Adding to setup.py, paper documentation, and further testing

--- tests/test_unambiguous_state_exclusion.py
+++ tests/test_unambiguous_state_exclusion.py
@@ -13,26 +13,26 @@ class TestUnambiguousStateExclusion(unittest.TestCase):
 
     def test_unambiguous_state_exclusion_one_state(self):
         """Unambiguous state exclusion for single state."""
-        rho = bell(0) * bell(0).conj().T
-        states = [rho]
+        mat = bell(0) * bell(0).conj().T
+        states = [mat]
 
         res = unambiguous_state_exclusion(states)
         self.assertEqual(np.isclose(res, 0), True)
 
     def test_unambiguous_state_exclusion_one_state_vec(self):
         """Unambiguous state exclusion for single vector state."""
-        rho = bell(0)
-        states = [rho]
+        vec = bell(0)
+        states = [vec]
 
         res = unambiguous_state_exclusion(states)
         self.assertEqual(np.isclose(res, 0), True)
 
     def test_unambiguous_state_exclusion_three_state(self):
         """Unambiguous state exclusion for three Bell state density matrices."""
-        rho1 = bell(0) * bell(0).conj().T
-        rho2 = bell(1) * bell(1).conj().T
-        rho3 = bell(2) * bell(2).conj().T
-        states = [rho1, rho2, rho3]
+        mat1 = bell(0) * bell(0).conj().T
+        mat2 = bell(1) * bell(1).conj().T
+        mat3 = bell(2) * bell(2).conj().T
+        states = [mat1, mat2, mat3]
         probs = [1 / 3, 1 / 3, 1 / 3]
 
         res = unambiguous_state_exclusion(states, probs)
@@ -40,10 +40,10 @@ class TestUnambiguousStateExclusion(unittest.TestCase):
 
     def test_unambiguous_state_exclusion_three_state_vec(self):
         """Unambiguous state exclusion for three Bell state vectors."""
-        rho1 = bell(0)
-        rho2 = bell(1)
-        rho3 = bell(2)
-        states = [rho1, rho2, rho3]
+        mat1 = bell(0)
+        mat2 = bell(1)
+        mat3 = bell(2)
+        states = [mat1, mat2, mat3]
         probs = [1 / 3, 1 / 3, 1 / 3]
 
         res = unambiguous_state_exclusion(states, probs)
@@ -52,9 +52,9 @@ class TestUnambiguousStateExclusion(unittest.TestCase):
     def test_invalid_unambiguous_state_exclusion_probs(self):
         """Invalid probability vector for unambiguous state exclusion."""
         with self.assertRaises(ValueError):
-            rho1 = bell(0) * bell(0).conj().T
-            rho2 = bell(1) * bell(1).conj().T
-            states = [rho1, rho2]
+            mat1 = bell(0) * bell(0).conj().T
+            mat2 = bell(1) * bell(1).conj().T
+            states = [mat1, mat2]
             unambiguous_state_exclusion(states, [1, 2, 3])
 
     def test_invalid_unambiguous_state_exclusion_states(self):
