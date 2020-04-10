20b9cd2d07064b8a1dd7512bc5cc4ce190a4f70f
Vincent Russo <vincentrusso1@gmail.com>
2020-04-10 11:12:38 -0400

    Trying to fix Sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -13,6 +13,9 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('../toqito'))
+sys.path.insert(0, os.path.abspath('../toqito/base'))
+sys.path.insert(0, os.path.abspath('../toqito/entanglement'))
 
 
 # -- Project information -----------------------------------------------------
