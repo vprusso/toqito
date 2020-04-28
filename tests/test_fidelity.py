68ecdf2a2b7af547c8ae2b76390ece95422514d5
Vincent Russo <vincentrusso1@gmail.com>
2020-04-27 20:38:20 -0400

    Fixing linter

--- tests/test_fidelity.py
+++ tests/test_fidelity.py
@@ -41,8 +41,8 @@ class TestFidelity(unittest.TestCase):
         """Test the fidelity between two non-identical states."""
         e_0, e_1 = ket(2, 0), ket(2, 1)
         rho = 3 / 4 * e_0 * e_0.conj().T + 1 / 4 * e_1 * e_1.conj().T
-        pi = 1 / 8 * e_0 * e_0.conj().T + 7 / 8 * e_1 * e_1.conj().T
-        self.assertEqual(np.isclose(fidelity(rho, pi), 0.774, rtol=1e-03), True)
+        sigma = 1 / 8 * e_0 * e_0.conj().T + 7 / 8 * e_1 * e_1.conj().T
+        self.assertEqual(np.isclose(fidelity(rho, sigma), 0.774, rtol=1e-03), True)
 
     def test_non_square(self):
         """Tests for invalid dim."""
