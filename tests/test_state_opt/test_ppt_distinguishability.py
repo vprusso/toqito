4713d2d8db738189304ac7dbfd961c79955773d0
Vincent Russo <vincentrusso1@gmail.com>
2020-07-12 22:55:13 -0400

    Fixing issue in swap function. Adding tests to cover case

--- tests/test_state_opt/test_ppt_distinguishability.py
+++ tests/test_state_opt/test_ppt_distinguishability.py
@@ -3,7 +3,7 @@ import numpy as np
 
 from toqito.perms import swap_operator
 from toqito.state_opt import ppt_distinguishability
-from toqito.states import bell
+from toqito.states import basis, bell
 
 
 def test_ppt_distinguishability_yyd_density_matrices():
@@ -126,6 +126,17 @@ def test_ppt_distinguishability_werner_hiding_pairs():
     \begin{equation}
         \frac{1}{2} + \frac{1}{n+1}
     \end{equation}
+
+    References:
+    [1]: Terhal, Barbara M., David P. DiVincenzo, and Debbie W. Leung.
+    "Hiding bits in Bell states."
+    Physical review letters 86.25 (2001): 5807.
+    https://arxiv.org/abs/quant-ph/0011042
+
+    [2]: Cosentino, Alessandro
+    "Quantum state local distinguishability via convex optimization".
+    University of Waterloo, Thesis
+    https://uwspace.uwaterloo.ca/handle/10012/9572
     """
     dim = 2
     sigma_0 = (np.kron(np.identity(dim), np.identity(dim)) + swap_operator(dim)) / (
@@ -140,6 +151,59 @@ def test_ppt_distinguishability_werner_hiding_pairs():
     expected_val = 1 / 2 + 1 / (dim + 1)
     res = ppt_distinguishability(states)
     np.testing.assert_equal(np.isclose(res, expected_val), True)
+
+
+def test_ppt_distinguishability_four_bell_states():
+    r"""
+    PPT distinguishing the four Bell states.
+
+    There exists a closed form formula for the probability with which one
+    is able to distinguish one of the four Bell states given with equal
+    probability when Alice and Bob have access to a resource state [1].
+
+    The resource state is defined by
+
+    ..math::
+
+    The closed form probability with which Alice and Bob can distinguish via
+    PPT measurements is given as follows
+
+    .. math::
+        \frac{1}{2} \left(1 + \sqrt{1 - \epsilon^2} \right).
+
+    This formula happens to be equal to LOCC and SEP as well for this case.
+    Refer to Theorem 5 in [1] for more details.
+
+    References:
+    [1]: Bandyopadhyay, Somshubhro, et al.
+    "Limitations on separable measurements by convex optimization."
+    IEEE Transactions on Information Theory 61.6 (2015): 3593-3604.
+    https://arxiv.org/abs/1408.6981
+    """
+    rho_1 = bell(0) * bell(0).conj().T
+    rho_2 = bell(1) * bell(1).conj().T
+    rho_3 = bell(2) * bell(2).conj().T
+    rho_4 = bell(3) * bell(3).conj().T
+
+    e_0, e_1 = basis(2, 0), basis(2, 1)
+    e_00 = np.kron(e_0, e_0)
+    e_11 = np.kron(e_1, e_1)
+
+    eps = 0.5
+    resource_state = np.sqrt((1 + eps) / 2) * e_00 + np.sqrt((1 - eps) / 2) * e_11
+    resource_state = resource_state * resource_state.conj().T
+
+    states = [
+        np.kron(rho_1, resource_state),
+        np.kron(rho_2, resource_state),
+        np.kron(rho_3, resource_state),
+        np.kron(rho_4, resource_state),
+    ]
+    probs = [1 / 4, 1 / 4, 1 / 4, 1 / 4]
+
+    res = ppt_distinguishability(states, probs)
+    exp_res = 1/2 * (1 + np.sqrt(1 - eps**2))
+    np.testing.assert_equal(np.isclose(res, exp_res), True)
 
 
 if __name__ == "__main__":
