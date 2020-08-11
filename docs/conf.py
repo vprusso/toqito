9112eea6a93bb39382f7f45e746df4669365dd4a
Vincent Russo <vincentrusso1@gmail.com>
2020-08-11 11:41:11 -0400

    Updating for 0.0.5

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ copyright = "2020, Vincent Russo"
 author = "Vincent Russo"
 
 # The full version, including alpha/beta/rc tags
-release = "0.0.4"
+release = "0.0.5"
 
 
 # -- General configuration ---------------------------------------------------
