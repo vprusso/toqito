157a55aa958c468bdaa519b8b87ad626e511e843
raphael <raph.koh@gmail.com>
2021-05-19 20:43:49 -0400

    Fix implementation of random_povm

--- toqito/random/random_povm.py
+++ toqito/random/random_povm.py
@@ -9,9 +9,10 @@ def random_povm(dim: int, num_inputs: int, num_outputs: int) -> np.ndarray:
     Examples
     ==========
 
-    We can generate a set of POVMs consisting of a specific dimension along with a given number of
-    measurement inputs and measurement outputs. As an example, we can construct a random set of
-    POVMs of dimension :math:`2` with :math:`2` inputs and :math:`2` outputs.
+    We can generate a set of `dim`-by-`dim` POVMs consisting of a specific dimension along with
+    a given number of measurement inputs and measurement outputs. As an example, we can
+    construct a random set of :math:`2`-by-:math:`2` POVMs of dimension with :math:`2` inputs
+    and :math:`2` outputs.
 
     >>> from toqito.random import random_povm
     >>> import numpy as np
@@ -40,13 +41,13 @@ def random_povm(dim: int, num_inputs: int, num_outputs: int) -> np.ndarray:
     .. [WIKPOVM] Wikipedia: POVM
         https://en.wikipedia.org/wiki/POVM
 
-    :param dim: The dimension of the measurements.
+    :param dim: The dimensions of the measurements.
     :param num_inputs: The number of inputs for the measurement.
     :param num_outputs: The number of outputs for the measurement.
-    :return: A set of POVMs of dimension :code:`dim`.
+    :return: A set of `dim`-by-`dim` POVMs of shape `(dim, dim, num_inputs, num_outputs)`.
     """
     povms = []
-    gram_vectors = np.random.normal(size=(dim, dim, num_inputs, num_outputs))
+    gram_vectors = np.random.normal(size=(num_inputs, num_outputs, dim, dim))
     for input_block in gram_vectors:
         normalizer = sum(
             [np.array(output_block).T.conj() @ output_block for output_block in input_block]
@@ -63,7 +64,7 @@ def random_povm(dim: int, num_inputs: int, num_outputs: int) -> np.ndarray:
             output_povms.append(internal.T.conj() @ internal)
         povms.append(output_povms)
 
-    # This allows us to index the POVMs as [d, d, num_inputs, num_outputs].
+    # This allows us to index the POVMs as [dim, dim, num_inputs, num_outputs].
     povms = np.swapaxes(np.array(povms), 0, 2)
     povms = np.swapaxes(povms, 1, 3)
 
