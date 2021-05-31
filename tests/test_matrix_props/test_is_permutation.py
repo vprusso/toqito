b23b2ddcadb7f4bbcedbaf36300e11d2af45eb06
Vanzilar <pbl1@cornell.edu>
2021-05-30 23:07:58 -0400

    Adding test cases for the is_permutation checked

---
+++ tests/test_matrix_props/test_is_permutation.py
@@ -0,0 +1,36 @@
+"""Test is_permutation."""
+import numpy as np
+
+from toqito.matrix_props import is_permutation
+from toqito.matrix_props import is_square
+
+def test_is_permutation():
+    """Test if matrix is a real permuattion matrix."""
+    mat = np.array([[1, 0, 0], [0, 0, 1], [0, 1, 0]])
+    np.testing.assert_equal(is_diagonal(mat), True)
+
+
+def test_is_not_permutation():
+    """Test non-permutation matrix."""
+    mat = np.array([[0, 0, 0, 9], [1, 0, 0, 0], [0, 1, 0, 0], [0,0,1,0]])
+    np.testing.assert_equal(is_diagonal(mat), False)
+
+def test_is_not_permutation():
+    """Test non-permutation matrix."""
+    mat = np.array([[0, 0, 1,0], [1, 0, 0, 0], [0, 1, 0, 0], [0,0,1,0]])
+    np.testing.assert_equal(is_diagonal(mat), False)
+
+
+def test_is_weird_permutation():
+    """Test on a weird non-permutation matrix that still adds up to 1."""
+    mat = np.array([[0.5, 0.5], [0.5, 0.5]])
+    np.testing.assert_equal(is_diagonal(mat), False)
+
+def test_is_weirder_permutation():
+    """Test on an evil non-permutation matrix that still adds up to 1."""
+    mat = np.array([[2, -1], [-1, 2]])
+    np.testing.assert_equal(is_diagonal(mat), False)
+
+
+if __name__ == "__main__":
+    np.testing.run_module_suite()
