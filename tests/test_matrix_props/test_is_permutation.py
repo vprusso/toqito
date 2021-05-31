d4c0f6c09bc84aed40d0f178d61430741486e0a9
Vanzilar <pbl1@cornell.edu>
2021-05-30 23:55:59 -0400

    Correcting a function call error
    Will need to find a way to get this working on my local version,

--- tests/test_matrix_props/test_is_permutation.py
+++ tests/test_matrix_props/test_is_permutation.py
@@ -7,29 +7,29 @@ from toqito.matrix_props import is_square
 def test_is_permutation():
     """Test if matrix is a real permuattion matrix."""
     mat = np.array([[1, 0, 0], [0, 0, 1], [0, 1, 0]])
-    np.testing.assert_equal(is_diagonal(mat), True)
+    np.testing.assert_equal(is_permutation(mat), True)
 
 
 def test_is_not_permutation():
     """Test non-permutation matrix."""
     mat = np.array([[0, 0, 0, 9], [1, 0, 0, 0], [0, 1, 0, 0], [0,0,1,0]])
-    np.testing.assert_equal(is_diagonal(mat), False)
+    np.testing.assert_equal(is_permutation(mat), False)
 
 def test_is_not_permutation():
     """Test non-permutation matrix."""
     mat = np.array([[0, 0, 1,0], [1, 0, 0, 0], [0, 1, 0, 0], [0,0,1,0]])
-    np.testing.assert_equal(is_diagonal(mat), False)
+    np.testing.assert_equal(is_permutation(mat), False)
 
 
 def test_is_weird_permutation():
     """Test on a weird non-permutation matrix that still adds up to 1."""
     mat = np.array([[0.5, 0.5], [0.5, 0.5]])
-    np.testing.assert_equal(is_diagonal(mat), False)
+    np.testing.assert_equal(is_permutation(mat), False)
 
 def test_is_weirder_permutation():
     """Test on an evil non-permutation matrix that still adds up to 1."""
     mat = np.array([[2, -1], [-1, 2]])
-    np.testing.assert_equal(is_diagonal(mat), False)
+    np.testing.assert_equal(is_permutation(mat), False)
 
 
 if __name__ == "__main__":
