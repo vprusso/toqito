387df952d10ba3a512cccad65cfa973be5b14852
Vanzilar <pbl1@cornell.edu>
2021-06-01 19:59:26 -0400

    Fix test function names and comments to be clearer
     and not duplicate function names so all tests are run

--- tests/test_matrix_props/test_is_permutation.py
+++ tests/test_matrix_props/test_is_permutation.py
@@ -3,27 +3,27 @@ import numpy as np
 
 from toqito.matrix_props import is_permutation
 
-def test_is_permutation():
-    """Test if matrix is a real permutation matrix."""
+def test_is_simple_permutation():
+    """Test if matrix is a simple permutation matrix."""
     mat = np.array([[1, 0, 0], [0, 0, 1], [0, 1, 0]])
     np.testing.assert_equal(is_permutation(mat), True)
 
 def test_is_not_permutation():
-    """Test non-permutation matrix."""
+    """Test non-permutation matrix wih one element being 9 !in set (0,1)."""
     mat = np.array([[0, 0, 0, 9], [1, 0, 0, 0], [0, 1, 0, 0], [0,0,1,0]])
     np.testing.assert_equal(is_permutation(mat), False)
 
-def test_is_not_permutation():
-    """Test non-permutation matrix."""
+def test_is_not_permutation_also():
+    """Test non-permutation matrix where the row and column check will fail."""
     mat = np.array([[0, 0, 1,0], [1, 0, 0, 0], [0, 1, 0, 0], [0,0,1,0]])
     np.testing.assert_equal(is_permutation(mat), False)
 
-def test_is_weird_permutation():
+def test_is_weird_not_permutation():
     """Test on a weird non-permutation matrix that still adds up to 1."""
     mat = np.array([[0.5, 0.5], [0.5, 0.5]])
     np.testing.assert_equal(is_permutation(mat), False)
 
-def test_is_weirder_permutation():
+def test_is_insidious_not_permutation():
     """Test on an insidious non-permutation matrix that still adds up to 1."""
     mat = np.array([[2, -1], [-1, 2]])
     np.testing.assert_equal(is_permutation(mat), False)
