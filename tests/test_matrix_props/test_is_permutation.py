9b1bba9594efc96633e5c776cb3ae282a6e21aeb
Vanzilar <pbl1@cornell.edu>
2021-06-10 20:53:23 -0400

    Rename functions
    also debating whether to add a 1-D permutation matrix check and if it is valid or not.

--- tests/test_matrix_props/test_is_permutation.py
+++ tests/test_matrix_props/test_is_permutation.py
@@ -8,33 +8,33 @@ def test_is_simple_permutation():
     mat = np.array([[1, 0, 0], [0, 0, 1], [0, 1, 0]])
     np.testing.assert_equal(is_permutation(mat), True)
 
-def test_is_not_square():
+def test_is_not_square_matrix():
     """Test non-square matrix and thus will fail."""
     mat = np.array([[1, 0, 0 ], [1, 0, 0], [0, 1, 0], [0,0,1]])
     np.testing.assert_equal(is_permutation(mat), False)
 
-def test_is_not_permutation():
-    """Test non-permutation matrix with one element being 9 !in set (0,1)."""
+def test_is_not_binary_value_matrix():
+    """Test a non-permutation matrix with one element being 9 which is not in set (0,1)."""
     mat = np.array([[0, 0, 0, 9], [1, 0, 0, 0], [0, 1, 0, 0], [0,0,1,0]])
     np.testing.assert_equal(is_permutation(mat), False)
 
-def test_is_not_permutation_row():
-    """Test non-permutation matrix where the row check will fail."""
+def test_is_matrix_with_nonunitary_row_sum():
+    """Test a non-permutation matrix where the unitary row sum check will fail."""
     mat = np.array([[0, 1, 1,0], [1, 0, 0, 0], [0, 0, 0, 1], [0,0,1,0]])
     np.testing.assert_equal(is_permutation(mat), False)
 
-def test_is_not_permutation_also():
-    """Test non-permutation matrix where the row and column check will fail."""
+def test_is_matrix_with_nonunitary_column_sum():
+    """Testa  non-permutation matrix where the unitary column sum check will fail."""
     mat = np.array([[0, 0, 1,0], [1, 0, 0, 0], [0, 1, 0, 0], [0,0,1,0]])
     np.testing.assert_equal(is_permutation(mat), False)
 
-def test_is_weird_not_permutation():
-    """Test on a weird non-permutation matrix that still adds up to 1."""
+def test_is_matrix_with_fractional_values_and_unitary_sums():
+    """Test a non-permutation matrix with fractional values that passes the unitary sum check."""
     mat = np.array([[0.5, 0.5], [0.5, 0.5]])
     np.testing.assert_equal(is_permutation(mat), False)
 
-def test_is_insidious_not_permutation():
-    """Test on an insidious non-permutation matrix that still adds up to 1."""
+def test_is_matrix_with_negative_values_and_unitary_sums():
+    """Test a non-permutation matrix with some negative values that passes the unitary sum check."""
     mat = np.array([[2, -1], [-1, 2]])
     np.testing.assert_equal(is_permutation(mat), False)
 
