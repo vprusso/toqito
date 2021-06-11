45e7e1b42c9e45a061cd258507debc0c7dd57268
Vanzilar <pbl1@cornell.edu>
2021-06-10 20:14:29 -0400

    Changed matrix notation, removed square check
    Changes the matrix notation from indicating index numbers to containing the actual element values as requested.
    Removed is_square check as requested. (If it is needed it can be restored later)

--- toqito/matrix_props/is_permutation.py
+++ toqito/matrix_props/is_permutation.py
@@ -1,6 +1,5 @@
 """Is matrix a permutation matrix."""
 import numpy as np
-from toqito.matrix_props import is_square
 
 def is_permutation(mat: np.ndarray) -> bool:
     r"""
@@ -12,16 +11,16 @@ def is_permutation(mat: np.ndarray) -> bool:
     Examples
     ==========
 
-    Consider the following matrix
+    Consider the following permutation matrix
 
     .. math::
         A = \begin{pmatrix}
-                1 & 2 & 3 \\
-                4 & 5 & 6 \\
-                7 & 8 & 9
+                1 & 0 & 0 \\
+                0 & 0 & 1 \\
+                0 & 1 & 0
             \end{pmatrix}
 
-    our function indicates that this is indeed a permutation matrix.
+    which is indeed a permutation matrix.
 
     >>> from toqito.matrix_props import is_permutation
     >>> import numpy as np
@@ -33,16 +32,20 @@ def is_permutation(mat: np.ndarray) -> bool:
 
     .. math::
         B = \begin{pmatrix}
-                1 & 2 & 3 \\
-                4 & 5 & 6 \\
-                7 & 8 & 9
+                1 & 0 & 0 \\
+                1 & 0 & 0 \\
+                1 & 0 & 0
             \end{pmatrix}
+
+    has 2 columns with all zero values and is thus not a 
+    permutation matrix.
 
     >>> from toqito.matrix_props import is_permutation
     >>> import numpy as np
     >>> B = np.array([[1, 0, 0], [1, 0, 0], [1, 0, 0]])
     >>> is_permutation(B)
     False
+
 
     References
     ==========
@@ -52,9 +55,6 @@ def is_permutation(mat: np.ndarray) -> bool:
     :param mat: The matrix to check.
     :return: Returns :code:`True` if the matrix is a permutation matrix and :code:`False` otherwise.
     """
-    if not is_square(mat):
-        return False
-
     for i in np.nditer(mat):
         if i not in (0, 1):
             return False
