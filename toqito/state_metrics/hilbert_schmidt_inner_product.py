648026d8b3ee3a03bd43a214c8d8c8eceaa491f4
Adithya Sireesh <adithyasireesh@dyn3199-162.wlan.ic.ac.uk>
2022-06-29 14:08:30 +0100

    Add Hilbert-Schmidt Inner Product function

--- toqito/state_metrics/hilbert_schmidt_inner_product.py
+++ toqito/state_metrics/hilbert_schmidt_inner_product.py
@@ -2,7 +2,7 @@
 import numpy as np
 
 
-def hilbert_schmidt_inner_product(A: np.ndarray, B: np.ndarray) -> float:
+def hilbert_schmidt_inner_product(A: np.ndarray, B: np.ndarray) -> complex:
     r"""
     Compute the Hilbert-Schmidt inner product between two matrices [WikHSO].
 
@@ -14,6 +14,8 @@ def hilbert_schmidt_inner_product(A: np.ndarray, B: np.ndarray) -> float:
         HS = (A|B) = Tr[A^\dagger B]
     
     where :math:`|B) = vec(B)` and :math:`(A|` is the dual vector to :math:`|A)`.
+    
+    Note: This function has been adapted from [Rigetti21]_.
 
     Examples
     ==========
@@ -31,10 +33,12 @@ def hilbert_schmidt_inner_product(A: np.ndarray, B: np.ndarray) -> float:
     ==========
     .. [WikHSO] Wikipedia: Hilbert-Schmidt operator.
         https://en.wikipedia.org/wiki/Hilbert%E2%80%93Schmidt_operator
+    .. [Rigetti21] Forest Benchmarking (Rigetti).
+        https://github.com/rigetti/forest-benchmarking
 
     :param A: An input matrix A.
     :param B: An input matrix B.
     :return: The Hilbert-Schmidt inner product between :code:`A` and :code:`B`.
     """
     hs_ip = np.trace(np.matmul(np.transpose(np.conj(A)), B))
-    return np.ndarray.item(np.real_if_close(hs_ip, 1000))
+    return hs_ip
