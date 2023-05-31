d04c36757bf080cbc54fef159e81fe30a0c0a2ab
georgios-ts <ts.giorgos@yahoo.gr>
2023-05-31 16:09:44 +0300

    Add `channel_dim`.
    This commit adds a helper function to compute the input, output and environment
    dimensions of a channel. It will be used in subsequent PRs to fix places where we
    try to retrieve the dimensions of the channel.

--- toqito/helper/__init__.py
+++ toqito/helper/__init__.py
@@ -5,3 +5,4 @@ from toqito.helper.update_odometer import update_odometer
 from toqito.helper.cvx_kron import cvx_kron
 from toqito.helper.npa_hierarchy import npa_constraints
 from toqito.helper.kp_norm import kp_norm
+from toqito.helper.channel_dim import channel_dim
