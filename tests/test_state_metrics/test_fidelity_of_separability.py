f9a6795f3b01508d1e10e21e5f2432f1328dbbff
Purva Thakre <purva.athakre@gmail.com>
2023-06-02 15:55:53 -0500

    code form arxiv

--- tests/test_state_metrics/test_fidelity_of_separability.py
+++ tests/test_state_metrics/test_fidelity_of_separability.py
@@ -2,36 +2,94 @@
 
 import numpy as np
 import pytest
-from toqito.states import basis
-from toqito.states import bell
+from toqito.states import bell, max_mixed
 from toqito.state_metrics import (
     state_ppt_extendible_fidelity, channel_ppt_extendible_fidelity)
+from toqito.matrix_ops import tensor
+
 
 # test mixed rho
-e_0, e_1 = basis(2, 0), basis(2, 1)
-mixed_density_matrix = 3 / 4 * e_0 * e_0.conj().T + 1 / 4 * e_1 * e_1.conj().T
+mixed_rho = 1/2 * bell(0) * bell(0).conj().T + 1 / 2 * bell(
+    3) * bell(3).conj().T
 
-# test pure rho
+# test pure entangled rho
 u = bell(0)
-pure_density_matrix = u * u.conj().T
+entangled_rho = u * u.conj().T
 
 # test bad density matrix
-bad_density_matrix = 1 / 2 * np.array([[1, 2], [3, 1]])
+bad_rho = 1 / 2 * np.array([[1, 2], [3, 1]])
+
+# test pure separable rho
+sep_rho = np.kron(
+        np.array([[1, 0], [0, 0]]),
+        np.array([[0, 0], [0, 1]])
+    )
+
+purification_state = tensor(
+        # System B:
+        np.array([[1, 0], [0, 0]]),
+        # System A:
+        np.array([[1, 0], [0, 0]]),
+        # System R:
+        np.array([[0, 0], [0, 1]])
+    )
 
 
-@pytest.mark.parametrize(
-    "test_func", [
-        state_ppt_extendible_fidelity, channel_ppt_extendible_fidelity]
-)
-@pytest.mark.parametrize("good_input", [
-    mixed_density_matrix, pure_density_matrix])
-def test_errors_raised(test_func, good_input):
-    """Tests for riased errors."""
-    with pytest.raises(
-            AssertionError,
-            match="Incorrect bipartite state dimensions provided."):
-        test_func(good_input, [2, 2, 2])
+def test_errors_state_SDP():
+    """Tests for riased errors in state SDP function."""
     with pytest.raises(
             ValueError,
             match="Provided input state is not a density matrix."):
-        test_func(bad_density_matrix, [2, 2])
+        state_ppt_extendible_fidelity(bad_rho, [2, 2])
+    with pytest.raises(
+            AssertionError,
+            match="For State SDP: require bipartite state dims."):
+        state_ppt_extendible_fidelity(sep_rho, [2, 2, 2])
+    with pytest.raises(
+            ValueError,
+            match="Provided input state is entangled."):
+        state_ppt_extendible_fidelity(entangled_rho, [2, 2])
+    with pytest.raises(
+            ValueError,
+            match="This function only works for pure states."):
+        state_ppt_extendible_fidelity(mixed_rho, [2, 2])
+
+
+def test_sdp_output():
+    """Test expected output of both the SDP functions."""
+    expected_value = 1
+    state_output_value = state_ppt_extendible_fidelity(sep_rho, [2, 2], 2)
+    assert np.isclose(expected_value, state_output_value)
+
+    channel_output_value = channel_ppt_extendible_fidelity(
+        purification_state, [2, 2, 2], 2)
+    assert np.isclose(expected_value, channel_output_value)
+
+
+# re-define states for channel SDP function
+bad_rho = 0.75*tensor(
+        # System B:
+        np.array([[1, 0], [0, 0]]),
+        # System A:
+        np.array([[1, 0], [0, 0]]),
+        # System R:
+        np.array([[0, 0], [0, 1]])
+    )
+
+mixed_rho = max_mixed(8, is_sparse=False)
+
+
+def test_errors_channel_SDP():
+    """Tests for riased errors in state SDP function."""
+    with pytest.raises(
+            ValueError,
+            match="Provided input state is not a density matrix."):
+        channel_ppt_extendible_fidelity(bad_rho, [2, 2, 2])
+    with pytest.raises(
+            AssertionError,
+            match="For Channel SDP: require tripartite state dims."):
+        channel_ppt_extendible_fidelity(purification_state, [2, 2])
+    with pytest.raises(
+            ValueError,
+            match="This function only works for pure states."):
+        channel_ppt_extendible_fidelity(mixed_rho, [2, 2, 2])
