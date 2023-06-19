3dc65c22afe64d509834dfefb5b150ed46ba35e4
smtsjhr <44889551+smtsjhr@users.noreply.github.com>
2023-06-19 00:37:24 -0700

    Update test_is_unextendible_product_basis.py
    Added docstrings and tests for functions returning Tiles and GenShifts UPB local state list.

--- tests/test_state_props/test_is_unextendible_product_basis.py
+++ tests/test_state_props/test_is_unextendible_product_basis.py
@@ -7,10 +7,51 @@ from toqito.state_props.is_unextendible_product_basis import is_unextendible_pro
 from toqito.state_props.is_unextendible_product_basis import item_partitions
 from toqito.matrix_ops import tensor
 from toqito.matrix_ops import inner_product
+from toqito.state_props import is_mutually_orthogonal
+from numpy.linalg import matrix_rank
 
 
 def tiles_local_state_list():
-    """Constructs Tiles UPB."""
+    """
+    Returns a list of arrays representing the local states of the Tiles UPB (unextendible product basis) [Ben99]_.
+
+    The Tiles UPB is a set of $5$ pure product states of the bipartite qutrit space :math:`\mathbb{C}^3\otimes\mathbb{C}^3` given by:
+
+    .. math::
+        \begin{align*}
+        |\psi_0\rangle &= \tfrac{1}{\sqrt{2}}|0\rangle \otimes \left(|0\rangle - |1\rangle \right), \\
+        |\psi_1\rangle &= \tfrac{1}{\sqrt{2}}\left(|0\rangle - |1\rangle \right) \otimes |2\rangle, \\
+        |\psi_2\rangle &= \tfrac{1}{\sqrt{2}}|2\rangle \otimes \left(|1\rangle - |2\rangle \right), \\
+        |\psi_3\rangle &= \tfrac{1}{\sqrt{2}}\left(|1\rangle - |2\rangle \right) \otimes |0\rangle, \\
+        |\psi_4\rangle &= \tfrac{1}{3}\left(|0\rangle + |1\rangle + |2\rangle \right) \otimes \left(|0\rangle + |1\rangle + |2\rangle \right).
+        \end{align*}
+
+    The product states of the Tiles UPB can be factored into two sets, :math:`\mathcal{C}_0$ and $\mathcal{C}_1`, of local qutrit states over the biparition :math:`\mathbb{C}^3\otimes \mathbb{C}^3` as follows.
+
+    The first party has states :math:`\mathcal{C}_0 = \{|\varphi_{0,j}\rangle\}_j` in :math:`\mathbb{C}^3`:
+
+    .. math::
+        \begin{align*}
+        |\varphi_{0, 0}\rangle &= |0\rangle , \\
+        |\varphi_{0, 1}\rangle &= \tfrac{1}{\sqrt{2}}\left(|0\rangle - |1\rangle \right), \\
+        |\varphi_{0, 2}\rangle &= |2\rangle , \\
+        |\varphi_{0, 3}\rangle &= \tfrac{1}{\sqrt{2}}\left(|1\rangle - |2\rangle \right), \\
+        |\varphi_{0, 4}\rangle &= \tfrac{1}{\sqrt{3}}\left(|0\rangle + |1\rangle + |2\rangle \right).
+        \end{align*}
+
+    The second party has states :math:`\mathcal{C}_1 = \{|\varphi_{1,j}\rangle\}_j` in :math:`\mathbb{C}^3`:
+
+    .. math::
+        \begin{align*}
+        |\varphi_{1, 0}\rangle &= \tfrac{1}{\sqrt{2}}\left(|0\rangle - |1\rangle \right), \\
+        |\varphi_{1, 1}\rangle &= |2\rangle,  \\
+        |\varphi_{1, 1}\rangle &= \tfrac{1}{\sqrt{2}}\left(|1\rangle - |2\rangle \right), \\
+        |\varphi_{1, 3}\rangle &= |0\rangle, \\
+        |\varphi_{1, 4}\rangle &= \tfrac{1}{\sqrt{3}}\left(|0\rangle + |1\rangle + |2\rangle \right).
+        \end{align*}
+
+    Here, these local collections :math:`\mathcal{C}_i1 of states are represented as two dimensional arrays of type `numpy.ndarray`, where the :math:`j1-th column of each array defines the local state :math:`|\varphi_{i,j}\rangle1 of the respective party. In this way, each array has the same number of columns corresponding to the number of product states :math:`n`.
+    """
     tiles_A = np.zeros([3, 5])
     tiles_A[:, 0] = [1, 0, 0]
     tiles_A[:, 1] = [1 / np.sqrt(2), -1 / np.sqrt(2), 0]
@@ -29,7 +70,57 @@ def tiles_local_state_list():
 
 
 def genshifts_local_state_list(parties: int):
-    """Constructs GenShifts UPB for odd number of parties."""
+    """
+    Returns a list of arrays representing the local states of the GenShifts UPB (unextendible product basis) for an odd number of parties. DiV00.
+
+    The GenShifts UPBs are a family of UPBs defined for an odd number :math:`m\geq 3` of parties each holding a qubit:
+    
+    .. math::
+        \mathcal{H} = \bigotimes_{i=1}^{m} \mathbb{C}^2.
+
+    The GenShifts UPB for :math:`m=2k-1` parties contains $2k$ product states which are comprised of specially chosen local qubit states. Although there is some freedom in how to define these local states, they must satisfy the following property. These local qubit states, say :math:`|\psi_j\rangle1 for :math:`1\leq j\leq k-11, are chosen such that all are neither identical nor orthogonal to each other or the computational basis states :math:`|0\rangle` and :math:`|1\rangle`. Moreover, states :math:`|\psi_j^\perp\rangle` for :math:`1\leq j\leq k-1` are also chosen to be orthogonal to their respective pairs so that :math:`\langle \psi_j^\perp  |\psi_j\rangle = 0`.
+
+    Having made proper choices for states :math:`|\psi_j\rangle` and :math:`|\psi_j^\perp\rangle`, for :math:`1\leq j\leq k-1`, the product states of the GenShifts UPB for :math:`2k-1` parties are given by:
+
+    .. math::
+        \begin{align*}
+            |\Psi_0\rangle &= |0, 0, \dots, 0\rangle \\
+            |\Psi_1\rangle &= |1,\psi_1, \psi_2, \dots, \psi_{k-1}, \psi_{k-1}^\perp, \dots, \psi_{1}^\perp \rangle \\
+            |\Psi_2\rangle &= |\psi_{1}^\perp, 1,\psi_1, \psi_2, \dots, \psi_{k-1}, \psi_{k-1}^\perp, \dots, \psi_{2}^\perp \rangle \\
+            &\vdots \\
+            |\Psi_{2k-1}\rangle &= |\psi_1, \psi_2, \dots, \psi_{k-1}^\perp, \psi_{k-2}^\perp, \dots, \psi_{1}^\perp , 1\rangle, \\
+        \end{align*}
+
+    With the exception of the first state :math:`|\Psi_0\rangle = |0, 0, \dots, 0\rangle`, the rest of the states are given by right-shifted cyclic permutations of :math:`|\Psi_1\rangle`.
+
+
+    In 'toqito', the particular choices made for the local states :math:`|\psi_j\rangle` and :math:`\psi_j^\perp\rangle` matches the implementation given in `QETLAB` (citeQETLAB). To be precise, consider the qubit states parameterized by an angle value
+
+    .. math::
+        |\theta(j)\rangle = \cos(j\frac{\pi}{2k})|0\rangle + \sin(j\frac{\pi}{2k})|1\rangle.
+
+    Then the states :math:`|\theta(j)\rangle1 defined for integers values in the range :math:`1\leq j \leq 2k`, can be uniquely paired as orthogonal states :math:`|\psi_j\rangle` and :math:`|\psi_j^\perp\rangle` given as:
+
+    .. math::
+        \begin{align*}
+        |\psi_j\rangle &= |\theta(2k-j)\rangle, \\
+        |\psi_j^{\perp}\rangle &= |\theta(j)\rangle, 
+        \end{align*}
+
+    for :math:`1 \leq j \leq k-1`.
+
+    For example, in the case of :math:`3` parties the GenShifts UPB states are given by the following :math:`4` product states:
+
+    .. math::
+        \begin{align*}
+            |\Psi_0\rangle &= |0, 0, 0\rangle \\
+            |\Psi_1\rangle &= |1, -, + \rangle \\
+            |\Psi_2\rangle &= |+, 1, - \rangle \\
+            |\Psi_3\rangle &= |-, +, 1 \rangle, \\
+        \end{align*}
+
+    where :math:`|+\rangle = \frac{1}{\sqrt{2}}(|0\rangle + |1\rangle)` and :math:`|-\rangle = \frac{1}{\sqrt{2}}(|0\rangle - |1\rangle)`.
+    """
     if (parties % 2 == 0) or parties == 1:
         raise ValueError("Input must be an odd int greater than 1.")
 
@@ -55,6 +146,79 @@ def genshifts_local_state_list(parties: int):
         state_list = state_list[-1:] + state_list[:-1]
 
     return upb
+
+
+def test_tiles_mutually_orthogonal():
+    tiles = tiles_local_state_list()
+    tiles_product_state_list = []
+    for i in range(5):
+        tiles_product_state = tensor(
+            np.array([tiles[0][:, i]]).T, np.array([tiles[1][:, i]]).T
+        )
+        tiles_product_state_list.append(tiles_product_state)
+    res = is_mutually_orthogonal(tiles_product_state_list)
+    expected_res = True
+    np.testing.assert_equal(res, expected_res)
+
+
+def test_tiles_incomplete_span():
+    tiles = tiles_local_state_list()
+    tiles_product_state_matrix = np.zeros([9, 5])
+    for i in range(5):
+        tiles_product_state = tensor(
+            np.array([tiles[0][:, i]]).T, np.array([tiles[1][:, i]]).T
+        )
+        print(tiles_product_state, np.shape(tiles_product_state))
+        tiles_product_state_matrix[:, [i]] = tiles_product_state
+    rank = matrix_rank(tiles_product_state_matrix)
+    res = rank < 9
+    expected_res = True
+    np.testing.assert_equal(res, expected_res)
+
+
+# Test parameter ranges over number of parties of the UPB, which must be odd integer greatar than 1.
+@pytest.mark.parametrize("num_parties", [3, 5, 7])
+def test_genshifts_mutually_orthogonal(num_parties):
+    num_states = num_parties + 1
+    genshifts = genshifts_local_state_list(num_parties)
+    # Initialize a list to populate with global product states.
+    genshifts_product_state_list = []
+    for i in range(num_states):
+        # Initialize the global product state with first tensor factor.
+        genshifts_product_state = np.array([genshifts[0][:, i]]).T
+        # Tensor remaining parties' states.
+        for party in range(1, num_parties):
+            genshifts_product_state = tensor(
+                genshifts_product_state, np.array([genshifts[party][:, i]]).T
+            )
+        genshifts_product_state_list.append(genshifts_product_state)
+    res = is_mutually_orthogonal(genshifts_product_state_list)
+    expected_res = True
+    np.testing.assert_equal(res, expected_res)
+
+
+# Test parameter ranges over number of parties of the UPB, which must be odd integer greatar than 1.
+@pytest.mark.parametrize("num_parties", [3, 5, 7])
+def test_genshifts_incomplete_span(num_parties):
+    num_states = num_parties + 1
+    global_dim = 2**num_parties
+    genshifts = genshifts_local_state_list(num_parties)
+    # Initialize a matrix to populate with global product states.
+    genshifts_product_state_matrix = np.zeros([global_dim, num_states])
+    for i in range(num_states):
+        # Initialize the global product state with first tensor factor.
+        genshifts_product_state = np.array([genshifts[0][:, i]]).T
+        # Tensor remaining parties' states.
+        for party in range(1, num_parties):
+            genshifts_product_state = tensor(
+                genshifts_product_state, np.array([genshifts[party][:, i]]).T
+            )
+        # Set i-th colum of matrix to i-th global state.
+        genshifts_product_state_matrix[:, [i]] = genshifts_product_state
+    rank = matrix_rank(genshifts_product_state_matrix)
+    res = rank < global_dim
+    expected_res = True
+    np.testing.assert_equal(res, expected_res)
 
 
 def test_is_unextendible_product_basis_input_empty_list():
