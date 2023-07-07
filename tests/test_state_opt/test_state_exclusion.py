cef9cdfa71787311b3f5894087c54844901e17e8
vrusso <vincentrusso1@gmail.com>
2023-07-07 03:50:39 -0400

    Adding to CHANGELOG and adding more test coverage.

--- tests/test_state_opt/test_state_exclusion.py
+++ tests/test_state_opt/test_state_exclusion.py
@@ -7,14 +7,20 @@ from toqito.states import bell
 
 def test_conclusive_state_exclusion_one_state_vec():
     """Conclusive state exclusion for single vector state."""
-    value, _ = state_exclusion(vectors=[bell(0)], probs=None)
-    np.testing.assert_equal(np.isclose(value, 1), True)
+    primal_value, _ = state_exclusion(vectors=[bell(0)], probs=None, primal_dual="primal")
+    np.testing.assert_equal(np.isclose(primal_value, 1), True)
+
+    dual_value, _ = state_exclusion(vectors=[bell(0)], probs=None, primal_dual="primal")
+    np.testing.assert_equal(np.isclose(dual_value, 1), True)
 
 
 def test_conclusive_state_exclusion_three_state():
     """Conclusive state exclusion for three Bell state vectors."""
-    value, _ = state_exclusion(vectors=[bell(0), bell(1), bell(2)], probs=[1/3, 1/3, 1/3])
-    np.testing.assert_equal(np.isclose(value, 0), True)
+    primal_value, _ = state_exclusion(vectors=[bell(0), bell(1), bell(2)], probs=[1/3, 1/3, 1/3])
+    np.testing.assert_equal(np.isclose(primal_value, 0), True)
+
+    dual_value, _ = state_exclusion(vectors=[bell(0), bell(1), bell(2)], probs=[1/3, 1/3, 1/3])
+    np.testing.assert_equal(np.isclose(dual_value, 0), True)
 
 
 if __name__ == "__main__":
