eb1a10b5c4d51e44ca95e48ec13fcc6ec58faefe
vrusso <vincentrusso1@gmail.com>
2023-07-07 04:45:59 -0400

    Fixing tests.

--- tests/test_state_opt/test_state_exclusion.py
+++ tests/test_state_opt/test_state_exclusion.py
@@ -2,28 +2,29 @@
 import numpy as np
 
 from toqito.state_opt import state_exclusion
-from toqito.states import bell
+from toqito.matrices import standard_basis
 
 
 def test_conclusive_state_exclusion_one_state_vec():
     """Conclusive state exclusion for single vector state."""
-    primal_value, _ = state_exclusion(vectors=[bell(0)], probs=None, primal_dual="primal")
-    np.testing.assert_equal(np.isclose(primal_value, 1), True)
-
-    dual_value, _ = state_exclusion(vectors=[bell(0)], probs=None, primal_dual="dual")
-    np.testing.assert_equal(np.isclose(dual_value, 1), True)
-
-
-def test_conclusive_state_exclusion_three_state():
-    """Conclusive state exclusion for three Bell state vectors."""
-    primal_value, _ = state_exclusion(
-        vectors=[bell(0), bell(1), bell(2)], probs=[1/3, 1/3, 1/3], primal_dual="primal"
-    )
+    e_0, e_1 = standard_basis(2)
+    trine = [
+        e_0,
+        1/2 * (-e_0 + np.sqrt(3) * e_1),
+        -1/2 * (e_0 + np.sqrt(3) * e_1),
+    ]
+    # No probabilities provided
+    primal_value, _ = state_exclusion(vectors=trine, probs=None, primal_dual="primal")
     np.testing.assert_equal(np.isclose(primal_value, 0), True)
 
-    dual_value, _ = state_exclusion(
-        vectors=[bell(0), bell(1), bell(2)], probs=[1/3, 1/3, 1/3], primal_dual="dual"
-    )
+    dual_value, _ = state_exclusion(vectors=trine, probs=None, primal_dual="dual")
+    np.testing.assert_equal(np.isclose(dual_value, 0), True)
+
+    # Probabilities provided
+    primal_value, _ = state_exclusion(vectors=trine, probs=[1/3, 1/3, 1/3], primal_dual="primal")
+    np.testing.assert_equal(np.isclose(primal_value, 0), True)
+
+    dual_value, _ = state_exclusion(vectors=trine, probs=[1/3, 1/3, 1/3], primal_dual="dual")
     np.testing.assert_equal(np.isclose(dual_value, 0), True)
 
 
