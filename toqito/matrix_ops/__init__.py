64f1ec9bccb8ad6976627dfb404152b87ea2bb25
vrusso <vincentrusso1@gmail.com>
2023-07-08 03:21:09 -0400

    Adding gram functions to init.

--- toqito/matrix_ops/__init__.py
+++ toqito/matrix_ops/__init__.py
@@ -4,3 +4,5 @@ from toqito.matrix_ops.vec import vec
 from toqito.matrix_ops.unvec import unvec
 from toqito.matrix_ops.inner_product import inner_product
 from toqito.matrix_ops.outer_product import outer_product
+from toqito.matrix_ops.vectors_from_gram_matrix import vectors_from_gram_matrix
+from toqito.matrix_ops.vectors_to_gram_matrix import vectors_to_gram_matrix
