5646c9556372da73ff053c08ce24a96b28b80105
vrusso <vincentrusso1@gmail.com>
2023-07-18 06:47:38 -0400

    Adding for test coverage.

--- tests/test_state_opt/test_state_exclusion.py
+++ tests/test_state_opt/test_state_exclusion.py
@@ -2,31 +2,32 @@
 import pytest
 import numpy as np
 
-from toqito.state_opt import state_exclusion
 from toqito.matrices import standard_basis
+from toqito.states import bell
+from toqito.state_opt import state_exclusion
 
 
-@pytest.mark.skip(reason="The MOSEK solver handles this gracefully, but CVXOPT does not.")
 def test_conclusive_state_exclusion_one_state_vec():
     """Conclusive state exclusion for single vector state."""
     e_0, e_1 = standard_basis(2)
-    trine = [
-        e_0,
-        1/2 * (-e_0 + np.sqrt(3) * e_1),
-        -1/2 * (e_0 + np.sqrt(3) * e_1),
+    states = [
+        1/np.sqrt(2) * (np.kron(e_0, e_0) + np.kron(e_1, e_1)),
+        1/np.sqrt(2) * (np.kron(e_0, e_0) - np.kron(e_1, e_1)),
+        1/np.sqrt(2) * (np.kron(e_0, e_1) + np.kron(e_1, e_0)),
+        1/np.sqrt(2) * (np.kron(e_0, e_1) - np.kron(e_1, e_0)),
     ]
     # No probabilities provided
-    primal_value, _ = state_exclusion(vectors=trine, probs=None, primal_dual="primal")
+    primal_value, _ = state_exclusion(vectors=states, probs=None, primal_dual="primal")
     np.testing.assert_equal(np.isclose(primal_value, 0), True)
 
-    dual_value, _ = state_exclusion(vectors=trine, probs=None, primal_dual="dual")
+    dual_value, _ = state_exclusion(vectors=states, probs=None, primal_dual="dual")
     np.testing.assert_equal(np.isclose(dual_value, 0), True)
 
     # Probabilities provided
-    primal_value, _ = state_exclusion(vectors=trine, probs=[1/3, 1/3, 1/3], primal_dual="primal")
+    primal_value, _ = state_exclusion(vectors=states, probs=[1/4, 1/4, 1/4, 1/4], primal_dual="primal")
     np.testing.assert_equal(np.isclose(primal_value, 0), True)
 
-    dual_value, _ = state_exclusion(vectors=trine, probs=[1/3, 1/3, 1/3], primal_dual="dual")
+    dual_value, _ = state_exclusion(vectors=states, probs=[1/4, 1/4, 1/4, 1/4], primal_dual="dual")
     np.testing.assert_equal(np.isclose(dual_value, 0), True)
 
 
