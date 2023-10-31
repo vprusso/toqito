87d7e82512875dfd48440be300f147ad296356d4
Purva Thakre <purva.athakre@gmail.com>
2023-10-31 08:05:00 -0500

    fix https://github.com/vprusso/toqito/pull/205#issuecomment-1785522298

--- toqito/matrices/gell_mann.py
+++ toqito/matrices/gell_mann.py
@@ -117,6 +117,6 @@ def gell_mann(ind: int, is_sparse: bool = False) -> np.ndarray | scipy.sparse.cs
         raise ValueError("Gell-Mann index values can only be values from 0 to 8 (inclusive).")
 
     if is_sparse:
-        gm_op = scipy.sparse.csr_matrix(gm_op)
+        gm_op = scipy.sparse.csr_matrix(gm_op)  # pylint: disable=redefined-variable-type
 
     return gm_op
