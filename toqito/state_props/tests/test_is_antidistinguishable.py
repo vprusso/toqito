7df247ac8ace65190a97bbe16bbaa6d4b877159a
Vincent Russo <vincentrusso1@gmail.com>
2023-11-24 22:32:29 -0500

    Update toqito/state_props/tests/test_is_antidistinguishable.py
    Co-authored-by: Purva Thakre <66048318+purva-thakre@users.noreply.github.com>

--- toqito/state_props/tests/test_is_antidistinguishable.py
+++ toqito/state_props/tests/test_is_antidistinguishable.py
@@ -6,11 +6,11 @@ from toqito.states import bell, trine
 from toqito.state_props import is_antidistinguishable
 
 
-@pytest.mark.parametrize("states, expected_result", [
+@pytest.mark.parametrize("states", [
     # The Bell states are known to be antidistinguishable.
-    ([bell(0), bell(1), bell(2), bell(3)], True),
+    ([bell(0), bell(1), bell(2), bell(3)]),
     # The trine states are known to be antidistinguishable.
-    ([trine()[0], trine()[1], trine()[2]], True),
+    ([trine()[0], trine()[1], trine()[2]]),
 ])
-def test_is_antidistinguishable(states, expected_result):
-    np.testing.assert_equal(is_antidistinguishable(states), expected_result)
+def test_is_antidistinguishable(states):
+   assert is_antidistinguishable(states)
