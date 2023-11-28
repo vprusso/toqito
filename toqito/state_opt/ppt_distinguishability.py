4f9414f11ecc15b33f14d882710cafe831c94f15
vrusso <vincentrusso1@gmail.com>
2023-11-27 22:54:06 -0500

    Fixing bug in state exclusion code.

--- toqito/state_opt/ppt_distinguishability.py
+++ toqito/state_opt/ppt_distinguishability.py
@@ -279,8 +279,8 @@ def dual_problem(
         )
 
     problem = cvxpy.Problem(objective, constraints)
-    sol_default = problem.solve()
+    sol_default = problem.solve(solver=cvxpy.MOSEK)
 
-    # print(np.around(y_var.value, decimals=3))
+    print(np.around(y_var.value, decimals=4))
 
     return sol_default
