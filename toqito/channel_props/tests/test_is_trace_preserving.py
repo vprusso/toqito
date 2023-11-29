90b71fd8e4bec89e9f76d741300b8736503abd6c
Purva Thakre <purva.athakre@gmail.com>
2023-11-29 17:24:32 -0600

    use pytest parametrize + add tests

--- toqito/channel_props/tests/test_is_trace_preserving.py
+++ toqito/channel_props/tests/test_is_trace_preserving.py
@@ -1,18 +1,17 @@
 """Tests for is_trace_preserving."""
 import numpy as np
+import pytest
 
 from toqito.channel_props import is_trace_preserving
 from toqito.channels import depolarizing
 
-
-def test_is_trace_preserving_kraus_false():
-    """Verify non-trace preserving channel as Kraus ops as False."""
-    unitary_mat = np.array([[1, 1], [-1, 1]]) / np.sqrt(2)
-    kraus_ops = [[np.identity(2), np.identity(2)], [unitary_mat, -unitary_mat]]
-
-    np.testing.assert_equal(is_trace_preserving(kraus_ops), False)
+unitary_mat = np.array([[1, 1], [-1, 1]]) / np.sqrt(2)
+kraus_ops = [[np.identity(2), np.identity(2)], [unitary_mat, -unitary_mat]]
 
 
-def test_is_trace_preserving_choi_true():
-    """Verify Choi matrix of the depolarizing map is trace preserving."""
-    np.testing.assert_equal(is_trace_preserving(depolarizing(2)), True)
+@pytest.mark.parametrize("input_unitary, expected_result, dims", [
+    (kraus_ops, False, None), (depolarizing(2), True, None), (depolarizing(4), True, 4), (depolarizing(4), False, 2)])
+def test_is_trace_prserving(input_unitary, expected_result, dims):
+    """Test function works as expected."""
+    assert is_trace_preserving(input_unitary, dim = dims) == expected_result
+
