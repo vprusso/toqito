45d73ba0b4ac36b702966d6505773c4f9c18b812
vrusso <vincentrusso1@gmail.com>
2023-12-03 11:30:56 -0500

    Fixing broken test.

--- toqito/matrix_props/tests/test_is_linearly_independent.py
+++ toqito/matrix_props/tests/test_is_linearly_independent.py
@@ -7,7 +7,7 @@ from toqito.matrix_props import is_linearly_independent
 
 @pytest.mark.parametrize("vectors, expected_result", [
     ([np.array([[1], [0], [1]]), np.array([[1], [1], [0]]), np.array([[0], [0], [1]])], True), 
-    ([np.array([[0], [1], [2]]), np.array([[[1], [2], [3]]]), np.array([[3], [5], [7]])], False), 
+    ([np.array([[0], [1], [2]]), np.array([[1], [2], [3]]), np.array([[3], [5], [7]])], False), 
 ])
 def test_is_linearly_independent(vectors, expected_result):
     np.testing.assert_equal(is_linearly_independent(vectors), expected_result)
