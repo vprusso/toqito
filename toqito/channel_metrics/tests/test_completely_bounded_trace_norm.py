d06a363d033055c35e05b3c9e38de94176d200ac
Purva Thakre <purva.athakre@gmail.com>
2023-12-05 08:34:54 -0600

    test_cb_trace_norm

--- toqito/channel_metrics/tests/test_completely_bounded_trace_norm.py
+++ toqito/channel_metrics/tests/test_completely_bounded_trace_norm.py
@@ -6,33 +6,28 @@ from toqito.channel_metrics import completely_bounded_trace_norm
 from toqito.channel_ops import kraus_to_choi
 from toqito.channels.dephasing import dephasing
 
-
-def test_cb_trace_norm_quantum_channel():
-    """The diamond norm of a quantum channel is 1."""
-    phi = dephasing(2)
-    assert completely_bounded_trace_norm(phi) == 1
-
-
-def test_cb_trace_norm_CP():
-    """Test for the diamond norm of a CP map."""
-    non_normalized_depolarizing_array = np.eye(4)
-    assert completely_bounded_trace_norm(non_normalized_depolarizing_array) == 4.0
+# things required for cb trace norm in terms of the eigenvalues of U
+U = 1 / np.sqrt(2) * np.array([[1, 1], [-1, 1]])  # Hadamard gate
+phi = kraus_to_choi([[np.eye(2), np.eye(2)], [U, -U]])
+lam, _ = np.linalg.eig(U)
+dist = np.abs(lam[:, None] - lam[None, :])  # all to all distance
+diameter = np.max(dist)
 
 
-def test_cb_trace_norm_unitaries_channel():
-    """Test the CB trace norm of phi.
+@pytest.mark.parametrize("test_input, expected", [
+    # The diamond norm of a quantum channel is 1
+    (dephasing(2), 1),
+    # the diamond norm of a CP map
+    (np.eye(4), 4.0),
+    # unitaries channel, phi, diameter in terms of the eigenvalues of U
+    (phi, diameter)])
+def test_cb_trace_norm(test_input, expected):
+    """Test function works as expected for valid inputs."""
+    calculated_value = completely_bounded_trace_norm(test_input)
+    assert abs(calculated_value - expected) <= 1e-3
 
-    The diamond norm of phi = id- U id U* is the diameter of the smallest circle that contains the eigenvalues of U.
-    """
-    U = 1 / np.sqrt(2) * np.array([[1, 1], [-1, 1]])  # Hadamard gate
-    phi = kraus_to_choi([[np.eye(2), np.eye(2)], [U, -U]])
-    lam, _ = np.linalg.eig(U)
-    dist = np.abs(lam[:, None] - lam[None, :])  # all to all distance
-    diameter = np.max(dist)
-    assert np.isclose(completely_bounded_trace_norm(phi), diameter, atol=1e-3)
 
-
-def test_cb_trace_norm_non_square():
+def test_cb_trace_norm_invalid_input():
     """Non-square inputs for cb trace norm."""
     with pytest.raises(
         ValueError,
