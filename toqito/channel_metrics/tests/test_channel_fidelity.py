02faf3d381950c8b23b48da0ef144d5fdc98af09
Purva Thakre <purva.athakre@gmail.com>
2023-12-05 08:25:29 -0600

    test_channel_fidelity

--- toqito/channel_metrics/tests/test_channel_fidelity.py
+++ toqito/channel_metrics/tests/test_channel_fidelity.py
@@ -8,29 +8,24 @@ from toqito.channels import dephasing, depolarizing
 dephasing_channel = dephasing(4)
 depolarizing_channel = depolarizing(4)
 
-
-def test_channel_fidelity_same_channel():
-    """The fidelity of identical channels should yield 1."""
-    assert np.isclose(channel_fidelity(dephasing_channel, dephasing_channel), 1, atol=1e-3)
-
-
-def test_channel_fidelity_different_channel():
-    """Calculate the channel fidelity of different channels."""
-    assert np.isclose(channel_fidelity(dephasing_channel, depolarizing_channel), 1 / 2, atol=1e-3)
-
-
-def test_channel_fidelity_inconsistent_dims():
-    """Inconsistent dimensions between Choi matrices."""
-    with pytest.raises(
-        ValueError, match="The Choi matrices provided should be of equal dimension."
-    ):
-        small_dim_depolarizing_channel = depolarizing(2)
-        channel_fidelity(depolarizing_channel, small_dim_depolarizing_channel)
+@pytest.mark.parametrize("input1, input2, expected_value", [
+    # fidelity of identical channels
+    (dephasing_channel, dephasing_channel, 1),
+    # fidelity of different channels
+    (dephasing_channel, depolarizing_channel, 1/2)])
+def test_channel_fidelity(input1, input2, expected_value):
+    """Test functions works as expected for valid inputs."""
+    calculated_value = channel_fidelity(input1, input2)
+    assert pytest.approx(expected_value, 1e-3) == calculated_value
 
 
-def test_channel_fidelity_non_square():
-    """Non-square inputs for channel fidelity."""
-    with pytest.raises(ValueError, match="The Choi matrix provided must be square."):
-        choi_1 = np.array([[1, 2, 3], [4, 5, 6]])
-        choi_2 = np.array([[1, 2, 3], [4, 5, 6]])
-        channel_fidelity(choi_1, choi_2)
+@pytest.mark.parametrize("input1, input2, expected_msg", [
+    # Inconsistent dimensions between Choi matrices
+    (depolarizing_channel, depolarizing(2), "The Choi matrices provided should be of equal dimension."),
+    # Non-square inputs for channel fidelity
+    (np.array([[1, 2, 3], [4, 5, 6]]), np.array([[1, 2, 3], [4, 5, 6]]), "The Choi matrix provided must be square.")])
+def test_channel_fidelity_raises_error(input1, input2, expected_msg):
+    """Test functions works as expected for valid inputs."""
+    with pytest.raises(ValueError, match = expected_msg):
+        channel_fidelity(input1, input2)
+
