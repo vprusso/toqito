4ec93ba88f6a07640a8bd25b48e3fc8160e72ef3
Purva Thakre <purva.athakre@gmail.com>
2023-12-13 18:02:12 -0600

    when dim is None

--- toqito/state_props/tests/test_sk_vec_norm.py
+++ toqito/state_props/tests/test_sk_vec_norm.py
@@ -20,3 +20,10 @@ def test_sk_norm_maximally_entagled_state_with_dim(n, k, dim):
     v_vec = max_entangled(n)
     res = sk_vector_norm(v_vec, k=k, dim=dim)
     assert np.isclose(res, 1.0)
+
+@pytest.mark.parametrize("n, k, expected_result", [(4, 2, 0.7), (5, 2, 0.63)])
+def test_sk_norm_maximally_entagled_state_with_none_dim(n, k, expected_result):
+    """The S(k)-norm of the maximally entagled state dim = None."""
+    v_vec = max_entangled(n)
+    res = sk_vector_norm(v_vec, k=k, dim=None)
+    assert np.isclose(res, expected_result, atol=0.01)
