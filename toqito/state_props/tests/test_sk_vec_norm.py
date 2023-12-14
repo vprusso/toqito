3e123d0ea3680452abc56741faae92f6983e8db4
Purva Thakre <66048318+purva-thakre@users.noreply.github.com>
2023-12-14 07:20:30 -0600

    Missing tests: `toqito/state_props/sk_vec_norm.py` (#366)
    * remove MATLAB use case scenario
    
    * add test allowing input dim
    
    * ruff failure
    
    * pylint failure
    
    * when dim is None

--- toqito/state_props/tests/test_sk_vec_norm.py
+++ toqito/state_props/tests/test_sk_vec_norm.py
@@ -11,5 +11,19 @@ def test_sk_norm_maximally_entagled_state(n, k):
     """The S(k)-norm of the maximally entagled state."""
     v_vec = max_entangled(n)
     res = sk_vector_norm(v_vec, k=k)
+    assert np.isclose(res, np.sqrt(k / n))
 
-    np.testing.assert_equal(np.isclose(res, np.sqrt(k / n)), True)
+
+@pytest.mark.parametrize("n, k, dim", [(4, 2, 1), (4, 2, [1]), (5, 2, 1), (5, 2, [1])])
+def test_sk_norm_maximally_entagled_state_with_dim(n, k, dim):
+    """The S(k)-norm of the maximally entagled state where k> input_dim."""
+    v_vec = max_entangled(n)
+    res = sk_vector_norm(v_vec, k=k, dim=dim)
+    assert np.isclose(res, 1.0)
+
+@pytest.mark.parametrize("n, k, expected_result", [(4, 2, 0.7), (5, 2, 0.63)])
+def test_sk_norm_maximally_entagled_state_with_none_dim(n, k, expected_result):
+    """The S(k)-norm of the maximally entagled state dim = None."""
+    v_vec = max_entangled(n)
+    res = sk_vector_norm(v_vec, k=k, dim=None)
+    assert np.isclose(res, expected_result, atol=0.01)
