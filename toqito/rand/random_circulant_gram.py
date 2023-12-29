464adfa1f2c677ce53b3829a64431537d5fb7570
ankit-pn <miankitsingh@gmail.com>
2023-12-29 20:03:42 +0530

    fix: doc

--- toqito/rand/random_circulant_gram.py
+++ toqito/rand/random_circulant_gram.py
@@ -5,7 +5,6 @@ import numpy as np
 def random_circulant_gram(dim: int) -> np.ndarray:
     r"""Generate a random circulant Gram matrix of specified dimension.
 
-
     A circulant matrix is a square matrix where the elements of each row are identical to the elements of the
     previous row such that the elements in one row are relocated by 1 position (in a cyclic manner) compared
     to the previous row. The eigenvalues and eigenvectors of this matrix are derived from the Discrete
@@ -14,7 +13,8 @@ def random_circulant_gram(dim: int) -> np.ndarray:
     For more information on circulant matrices, see :cite:`WikiCirculantMat`.
 
     This function utilizes the normalized DFT, a variation of DFT with normalized basis vectors. This
-    variation alters computational requirements and offers a different view on signal transformations. For additional information, see :cite:`StanfordNormDFT`.
+    variation alters computational requirements and offers a different view on signal transformations.
+    For additional information, see :cite:`StanfordNormDFT`.
 
     The function creates a circulant matrix from a random diagonal matrix and the normalized DFT matrix.
     First, it generates a diagonal matrix with random non-negative entries. Next, it constructs the
