255a1bcad91c30522350ce494f28095d3cc72b73
ankit-pn <miankitsingh@gmail.com>
2023-12-29 10:44:25 +0530

    fix: fix test_random_circulant_gram.

--- toqito/rand/__init__.py
+++ toqito/rand/__init__.py
@@ -1,8 +1,8 @@
 """Generate random quantum states and measurements."""
-from toqito.rand.random_circulant_gram import random_circulant_gram
+from toqito.rand.random_unitary import random_unitary
 from toqito.rand.random_density_matrix import random_density_matrix
 from toqito.rand.random_ginibre import random_ginibre
 from toqito.rand.random_povm import random_povm
 from toqito.rand.random_state_vector import random_state_vector
 from toqito.rand.random_states import random_states
-from toqito.rand.random_unitary import random_unitary
+from toqito.rand.random_circulant_gram import random_circulant_gram
--- toqito/rand/tests/test_random_circulant_gram.py
+++ toqito/rand/tests/test_random_circulant_gram.py
@@ -1,6 +1,7 @@
 """Test random_circulant_gram."""
 import numpy as np
 import pytest
+from numpy.testing import assert_array_almost_equal, assert_equal
 
 from toqito.rand.random_circulant_gram import random_circulant_gram
 
@@ -18,21 +19,23 @@ from toqito.rand.random_circulant_gram import random_circulant_gram
         10,
     ],
 )
+
+
 def test_random_circulant_gram(dim):
     """Test for random_circulant_gram function."""
     # Generate a random circulant Gram matrix.
     circulant_matrix = random_circulant_gram(dim)
 
     # Ensure the matrix has the correct shape.
-    assert circulant_matrix.shape == (dim, dim)
+    assert_equal(circulant_matrix.shape, (dim, dim))
 
     # Check that the matrix is symmetric.
-    assert np.allclose(circulant_matrix, circulant_matrix.T)
+    assert_array_almost_equal(circulant_matrix, circulant_matrix.T)
 
     # Check that the matrix is real.
-    assert np.all(np.isreal(circulant_matrix))
+    assert_equal(np.isreal(circulant_matrix).all(), True)
 
-    # Check that the matrix is positive semi-definite.
-    # This is done by verifying all eigenvalues are non-negative.
+    # Check that the matrix is positive semi-definite by verifying
+    # all eigenvalues are non-negative.
     eigenvalues = np.linalg.eigvalsh(circulant_matrix)
-    assert np.all(eigenvalues >= 0)
+    assert_array_almost_equal((eigenvalues >= 0), True)
