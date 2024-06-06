a7381f9b28873a86d6d71573975c7216e624ff6f
Purva Thakre <purva.athakre@gmail.com>
2024-06-06 14:25:04 -0500

    remove full path due to pytest failure

--- toqito/channel_metrics/completely_bounded_spectral_norm.py
+++ toqito/channel_metrics/completely_bounded_spectral_norm.py
@@ -33,4 +33,4 @@ def completely_bounded_spectral_norm(phi: np.ndarray) -> float:
 
     """
     dual_as_array = np.asarray(dual_channel(phi))
-    return completely_bounded_trace_norm.completely_bounded_trace_norm(dual_as_array)
+    return completely_bounded_trace_norm(dual_as_array)
