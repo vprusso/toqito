f611e7e1c1c5c509f19a2e7cb8f6908e1b991c14
Purva Thakre <purva.athakre@gmail.com>
2024-07-01 20:39:17 -0500

    fixes Feature: Check if matrix is nonnegative #557

---
+++ toqito/matrix_props/is_nonnegative.py
@@ -0,0 +1,31 @@
+"""Check if matrix is nonnegative."""
+
+import numpy as np
+
+
+def is_nonnegative(input_mat: np.ndarray) -> bool:
+    r"""Check if the matrix is nonnegative.
+
+    When all the entries in the matrix are larger than or equal to zero the matrix of interest is a
+    nonnegative matrix :cite:`WikiNonNegative`.
+
+
+    Examples
+    ==========
+    We expect an identity matrix to be nonnegative.
+
+    >>> import numpy as np
+    >>> from toqito.matrix_props import is_nonnegative
+    >>> id_matrix = np.identity(3)
+
+    References
+    ==========
+    .. bibliography::
+        :filter: docname in docnames
+
+
+    input_mat: np.ndarray
+        Matrix of interest.
+
+    """
+    return np.all(input_mat >= 0)
