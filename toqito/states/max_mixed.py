152dae40c0c7d9a13538b31abcefc783685d560a
Purva Thakre <purva.athakre@gmail.com>
2024-07-01 20:39:25 -0500

    doctest failure

--- toqito/states/max_mixed.py
+++ toqito/states/max_mixed.py
@@ -52,8 +52,9 @@ def max_mixed(dim: int, is_sparse: bool = False) -> [np.ndarray, dia_array]:
 
     >>> from toqito.states import max_mixed
     >>> max_mixed(2, is_sparse=True) # doctest: +ELLIPSIS, +NORMALIZE_WHITESPACE
-    <DIAgonal sparse array of dtype 'float64'
-        with 2 stored elements (1 diagonals) and shape (2, 2)>
+    <2x2 sparse array of type '<class 'numpy.float64'>'
+        with 2 stored elements in Compressed Sparse Row format>
+
 
     References
     ==========
