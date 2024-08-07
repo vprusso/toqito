59ed42627a96f90d867af30a1e757f02e967d5d5
Purva Thakre <purva.athakre@gmail.com>
2024-08-07 00:33:48 -0500

    tests and ruff failures

--- toqito/matrix_props/is_nonnegative.py
+++ toqito/matrix_props/is_nonnegative.py
@@ -1,13 +1,17 @@
-"""Check if matrix is nonnegative."""
+"""Check if matrix is nonnegative or doubly nonnegative."""
 
 import numpy as np
+
 from toqito.matrix_props import is_positive_semidefinite
+
 
 def is_nonnegative(input_mat: np.ndarray, mat_type: str = "nonnegative") -> bool:
     r"""Check if the matrix is nonnegative.
 
     When all the entries in the matrix are larger than or equal to zero the matrix of interest is a
     nonnegative matrix :cite:`WikiNonNegative`.
+
+    When a matrix is nonegative and positive semidefinite :cite:`WikiPosDef`, the matrix is doubly nonnegative.
 
 
     Examples
@@ -18,15 +22,22 @@ def is_nonnegative(input_mat: np.ndarray, mat_type: str = "nonnegative") -> bool
     >>> from toqito.matrix_props import is_nonnegative
     >>> is_nonnegative(np.identity(3))
     True
+    >>> is_nonnegative(np.identity(3), "doubly")
+    True
+    >>> is_nonnegative(np.identity(3), "nonnegative")
+    True
 
     References
     ==========
     .. bibliography::
         :filter: docname in docnames
 
+    :param input_mat: np.ndarray
+                    Matrix of interest.
+    :param mat_type: Type of nonnegative matrix. :code:`"nonnegative"` for a nonnegative matrix and :code:`"doubly"`
+                    for a doubly nonnegative matrix.
+    :raises TypeError: If something other than :code:`"doubly"`or :code:`"nonnegative"` is used for :code:`mat_type`.
 
-    input_mat: np.ndarray
-        Matrix of interest.
 
     """
     if mat_type == "nonnegative":
--- toqito/matrix_props/tests/test_is_nonnegative.py
+++ toqito/matrix_props/tests/test_is_nonnegative.py
@@ -1,4 +1,4 @@
-"""Tests for nonnegative matrix check function."""
+"""Tests for nonnegative and doubly nonnegative matrix check function."""
 
 import numpy as np
 import pytest
@@ -9,3 +9,12 @@ from toqito.matrix_props import is_nonnegative
 def test_identity():
     """Check an identity matrix is nonnegative as expected."""
     assert is_nonnegative(np.identity(3))
+    assert is_nonnegative(np.identity(3), "nonnegative")
+    assert is_nonnegative(np.identity(3), "doubly")
+
+
+@pytest.mark.parametrize("bad_type", [("l"), ("r"), (1), (), ("d")])
+def test_true(bad_type):
+    """Check if error raised correctly for invalid nonnegative matrix type."""
+    with pytest.raises(TypeError):
+        is_nonnegative(np.identity(3), bad_type)
