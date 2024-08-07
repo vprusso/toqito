2dbffce6913334a9f6ab56a31d50cd37ddb0e175
Purva Thakre <66048318+purva-thakre@users.noreply.github.com>
2024-08-07 08:41:05 -0500

    `matrix_props`: Check if stochastic or doubly stochastic (#723)
    * fixes Feature: Check if matrix stochastic #559
    
    * failures fix
    
    * trailing line
    
    * add doubly stohcastic function, unit tests and previous failures fix
    
    * recursive import + test coverage
    
    * Update toqito/matrix_props/is_doubly_stochastic.py
    
    * vincent's feedback: condense left, right functions
    
    * fix sphinx build errors
    
    * condensed is_stochastic
    
    * add details to docstrings
    
    * fix doctest failures
    
    * remove else
    
    * pragma no cover

--- toqito/matrix_props/__init__.py
+++ toqito/matrix_props/__init__.py
@@ -28,3 +28,5 @@ from toqito.matrix_props.is_linearly_independent import is_linearly_independent
 from toqito.matrix_props.is_nonnegative import is_nonnegative
 from toqito.matrix_props.is_positive import is_positive
 from toqito.matrix_props.positive_semidefinite_rank import positive_semidefinite_rank
+from toqito.matrix_props.is_stochastic import is_stochastic
+
