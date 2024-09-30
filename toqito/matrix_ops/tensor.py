57acefe3a69849a4201e26144955c58002196785
Tristan NEMOZ <tristan.nemoz@gmail.com>
2024-09-30 19:10:44 +0200

    Fast exponentiation for tensor product

--- toqito/matrix_ops/tensor.py
+++ toqito/matrix_ops/tensor.py
@@ -135,15 +135,22 @@ def tensor(*args) -> np.ndarray:
     # Tensor product one matrix `n` times with itself.
     if len(args) == 2 and isinstance(args[1], int):
         num_tensor = args[1]
+        if num_tensor == 0:
+            return None
         if num_tensor == 1:
             return args[0]
-        if num_tensor == 2:
-            return np.kron(args[0], args[0])
-        if num_tensor >= 3:
-            result = np.kron(args[0], args[0])
-            for _ in range(2, num_tensor):
-                result = np.kron(result, args[0])
-        return result
+        mat = args[0]
+        tensored_twice = np.kron(mat, mat)
+
+        def fast_exp(matrix, q):
+            if q == 1:
+                return matrix
+            tmp = fast_exp(tensored_twice, q >> 1)
+            if q & 1:
+                return np.kron(mat, tmp)
+            return tmp
+
+        return fast_exp(mat, num_tensor)
 
     # Tensor product between two or more matrices.
     if len(args) == 2:
