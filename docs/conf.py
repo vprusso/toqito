5a7b1b5351c39acbb13895284a79db367090c0f8
vprusso <vincentrusso1@gmail.com>
2024-10-06 08:15:34 -0400

    Changing from wagtail to rtd theme.

--- docs/conf.py
+++ docs/conf.py
@@ -43,7 +43,7 @@ extensions = [
     "sphinx.ext.githubpages",
     "sphinx.ext.napoleon",
     "sphinxcontrib.bibtex",
-    "sphinx_wagtail_theme",
+    "sphinx_rtd_theme",
 ]
 
 bibtex_bibfiles = ["refs.bib"]
@@ -103,7 +103,7 @@ exclude_patterns = ["_build", "_templates", "Thumbs.db", ".DS_Store"]
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 
-html_theme = "sphinx_wagtail_theme"
+html_theme = "sphinx_rtd_theme"
 html_logo = "figures/logo.svg"
 html_favicon = "figures/favicon.ico"
 
@@ -112,10 +112,6 @@ html_favicon = "figures/favicon.ico"
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
 html_css_files = ["custom.css"]
-
-# This github_url will point to the appropriate page in the default branch.
-# Ex: Getting Started -> https://github.com/vprusso/toqito/blob/master/docs/getting_started.rst
-html_theme_options = dict(github_url="https://github.com/vprusso/toqito/blob/master/docs/")
 
 # Show in footer when the docs were last updated.
 html_last_updated_fmt = "%b %d, %Y"
