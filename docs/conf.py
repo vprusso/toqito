d9034068dbaddef15333eeeb79676f9699f60483
Purva Thakre <purva.athakre@gmail.com>
2024-10-07 18:28:49 -0500

    add furo

--- docs/conf.py
+++ docs/conf.py
@@ -103,15 +103,15 @@ exclude_patterns = ["_build", "_templates", "Thumbs.db", ".DS_Store"]
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 
-html_theme = "sphinx_rtd_theme"
+html_theme = "furo"
 html_logo = "figures/logo.svg"
 html_favicon = "figures/favicon.ico"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
-html_css_files = ["custom.css"]
+# html_static_path = ["_static"]
+# html_css_files = ["custom.css"]
 
 # Show in footer when the docs were last updated.
 html_last_updated_fmt = "%b %d, %Y"
