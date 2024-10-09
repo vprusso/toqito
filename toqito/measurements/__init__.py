1a98dd14c18880e597561b19dfc3b839af41fbd5
Shivansh Mittal <shivansh20128@iiitd.ac.in>
2024-10-09 23:22:20 +0530

    updated docstring for measurements

--- toqito/measurements/__init__.py
+++ toqito/measurements/__init__.py
@@ -1,4 +1,4 @@
-"""Measurements."""
+"""Measurements is a set of modules that are used to compute a set of measurements from an ensemble."""
 
 from toqito.measurements.pretty_good_measurement import pretty_good_measurement
 from toqito.measurements.pretty_bad_measurement import pretty_bad_measurement
