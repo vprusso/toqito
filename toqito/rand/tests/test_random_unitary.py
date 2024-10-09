243b7e73896ca19f8fe57381e819dffc06ef053b
Tristan NEMOZ <tristan.nemoz@gmail.com>
2024-10-09 21:41:35 +0200

    random_unitary

--- toqito/rand/tests/test_random_unitary.py
+++ toqito/rand/tests/test_random_unitary.py
@@ -1,6 +1,7 @@
 """Test random_unitary."""
-
+import numpy as np
 import pytest
+from numpy.ma.testutils import assert_array_almost_equal
 
 from toqito.matrix_props import is_unitary
 from toqito.rand import random_unitary
@@ -32,3 +33,28 @@ def test_non_square_dims(dim_n, dim_m, is_real):
     if dim_n != dim_m:
         with pytest.raises(ValueError, match="Unitary matrix must be square."):
             random_unitary(dim=[dim_n, dim_m], is_real=is_real)
+
+@pytest.mark.parametrize(
+    "dim, is_real, expected",
+    [
+        (
+            2,
+            False,
+            np.array([
+                [0.51345691+0.6924564j, 0.38709015+0.32715034j],
+                [0.16581665-0.47892689j, -0.08796944+0.85755189j]
+            ])
+        ),
+        (
+            2,
+            True,
+            np.array([
+                [0.95160818, -0.30731397],
+                [0.30731397,  0.95160818]
+            ])
+        ),
+    ]
+)
+def test_seed(dim, is_real, expected):
+    mat = random_unitary(dim=dim, is_real=is_real, seed=123)
+    assert_array_almost_equal(mat, expected)
