46bb8cdcdfebc9d9b322a121a99b701161b50045
vprusso <vincentrusso1@gmail.com>
2024-10-10 08:29:35 -0400

    Weird docstring fix.

--- toqito/matrices/__init__.py
+++ toqito/matrices/__init__.py
@@ -1,5 +1,6 @@
 """Matrices."""
 
+from toqito.matrices.operator_sinkhorn import operator_sinkhorn
 from toqito.matrices.standard_basis import standard_basis
 from toqito.matrices.gen_pauli_x import gen_pauli_x
 from toqito.matrices.gen_pauli_z import gen_pauli_z
@@ -11,4 +12,3 @@ from toqito.matrices.gen_pauli import gen_pauli
 from toqito.matrices.hadamard import hadamard
 from toqito.matrices.pauli import pauli
 from toqito.matrices.cyclic_permutation_matrix import cyclic_permutation_matrix
-from toqito.matrices.operator_sinkhorn import operator_sinkhorn
