1b1ecc71d7f3d987626fa11d5d563371ea2d52a0
Shivansh Mittal <shivansh20128@iiitd.ac.in>
2024-10-16 17:03:52 +0530

    support for no perturbations

--- toqito/matrix_ops/perturb_vectors.py
+++ toqito/matrix_ops/perturb_vectors.py
@@ -15,8 +15,11 @@ def perturb_vectors(vectors: list[np.ndarray], eps: float = 0.1) -> list[np.ndar
     """
     perturbed_vectors: list[np.ndarray] = []
     for i, v in enumerate(vectors):
-        perturbed_vectors.append(v + np.random.randn(v.shape[0]) * eps)
+        if eps == 0:
+            perturbed_vectors.append(v)
+        else:
+            perturbed_vectors.append(v + np.random.randn(v.shape[0]) * eps)
 
-        # Normalize the vectors after perturbing them.
-        perturbed_vectors[i] = perturbed_vectors[i] / np.linalg.norm(perturbed_vectors[i])
+            # Normalize the vectors after perturbing them.
+            perturbed_vectors[i] = perturbed_vectors[i] / np.linalg.norm(perturbed_vectors[i])
     return np.array(perturbed_vectors)
