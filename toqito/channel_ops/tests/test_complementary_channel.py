a866e61abc542aea62f9a5cf6752dcbb6ffd7af9
Shivansh Mittal <shivansh20128@iiitd.ac.in>
2024-10-19 16:58:00 +0530

    update tests

--- toqito/channel_ops/tests/test_complementary_channel.py
+++ toqito/channel_ops/tests/test_complementary_channel.py
@@ -6,15 +6,15 @@ import pytest
 from toqito.channel_ops import complementary_channel
 
 # Define test cases for complementary map
-kraus_1 = np.array([[1, 0], [0, 1]]) / np.sqrt(2)
-kraus_2 = np.array([[0, 1], [1, 0]]) / np.sqrt(2)
-kraus_3 = np.array([[0, -1j], [1j, 0]]) / np.sqrt(2)
-kraus_4 = np.array([[1, 0], [0, -1]]) / np.sqrt(2)
+kraus_1 = np.array([[1, 0], [0, 0]])
+kraus_2 = np.array([[0, 1], [0, 0]])
+kraus_3 = np.array([[0, 0], [1, 0]])
+kraus_4 = np.array([[0, 0], [0, 1]])
 
 # Expected results for the complementary map
 expected_res_comp = [
-    np.array([[1, 0], [0, 1], [0, 0], [0, -1j]]),
-    np.array([[0, 1], [1, 0], [1j, 0], [0, -1]]),
+    np.array([[1, 0], [0, 1], [0, 0], [0, 0]]),
+    np.array([[0, 0], [0, 0], [1, 0], [0, 1]]),
 ]
 
 # Higher-dimensional Kraus operators (3x3)
@@ -22,45 +22,38 @@ kraus_5 = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]]) / np.sqrt(3)
 kraus_6 = np.array([[0, 1, 0], [1, 0, 1], [0, 1, 0]]) / np.sqrt(3)
 
 expected_res_comp_high_dim = [
-    np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1], [0, 1, 0], [1, 0, 0], [0, 0, 0]]),
-    np.array([[0, 1, 0], [1, 0, 1], [0, 1, 0], [0, 0, 0], [1, 0, 0], [1, 0, 0]]),
+    np.array([[1, 0, 0], [0, 1, 0]]) / np.sqrt(3),
+    np.array([[0, 1, 0], [1, 0, 1]]) / np.sqrt(3),
+    np.array([[0, 0, 1], [0, 1, 0]]) / np.sqrt(3),
 ]
 
 # Single Kraus operator (edge case)
 kraus_single = np.array([[1, 0], [0, 1]])
 
-expected_res_single = [np.array([[1, 0], [0, 1]])]  # Same as input
-
-# Large size (4x4 Kraus operator)
-kraus_large_1 = np.eye(4)
-kraus_large_2 = np.fliplr(np.eye(4))
-
-expected_res_large = [
-    np.vstack([np.eye(4)[i, :] for i in range(4)]),
-    np.vstack([np.fliplr(np.eye(4))[i, :] for i in range(4)]),
+expected_res_single =[
+    np.array([[1, 0]]),
+    np.array([[0, 1]]),
 ]
 
-# @pytest.mark.parametrize(
-#     "kraus_ops, expected",
-#     [
-#         # Test complementary_channel on a set of 2x2 Kraus operators (the ones you gave).
-#         ([kraus_1, kraus_2, kraus_3, kraus_4], expected_res_comp),
-#         # Test complementary_channel with higher-dimensional (3x3) Kraus operators.
-#         ([kraus_5, kraus_6], expected_res_comp_high_dim),
-#         # Test complementary_channel with a single Kraus operator (edge case).
-#         ([kraus_single], expected_res_single),
-#         # Test complementary_channel with large (4x4) Kraus operators.
-#         ([kraus_large_1, kraus_large_2], expected_res_large),
-#     ],
-# )
-# def test_complementary_channel(kraus_ops, expected):
-#     """Test complementary_channel works as expected for valid inputs."""
-#     calculated = complementary_channel(kraus_ops)
+@pytest.mark.parametrize(
+    "kraus_ops, expected",
+    [
+        # Test complementary_channel on a set of 2x2 Kraus operators (the ones you gave).
+        ([kraus_1, kraus_2, kraus_3, kraus_4], expected_res_comp),
+        # Test complementary_channel with higher-dimensional (3x3) Kraus operators.
+        ([kraus_5, kraus_6], expected_res_comp_high_dim),
+        # Test complementary_channel with a single Kraus operator (edge case).
+        ([kraus_single], expected_res_single),
+    ],
+)
+def test_complementary_channel(kraus_ops, expected):
+    """Test complementary_channel works as expected for valid inputs."""
+    calculated = complementary_channel(kraus_ops)
 
-#     # Compare the shapes first to debug broadcasting issues
-#     assert len(calculated) == len(expected), "Mismatch in number of Kraus operators"
-#     for calc_op, exp_op in zip(calculated, expected):
-#         assert np.isclose(calc_op, exp_op, atol=1e-6).all()
+    # Compare the shapes first to debug broadcasting issues
+    assert len(calculated) == len(expected), "Mismatch in number of Kraus operators"
+    for calc_op, exp_op in zip(calculated, expected):
+        assert np.isclose(calc_op, exp_op, atol=1e-6).all()
 
 # @pytest.mark.parametrize(
 #     "kraus_ops",
