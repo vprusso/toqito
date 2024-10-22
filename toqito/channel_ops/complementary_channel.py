d5b9517dda1c712ea498666e06748fda3574c02b
Shivansh Mittal <shivansh20128@iiitd.ac.in>
2024-10-22 19:40:47 +0530

    adding docstring example

--- toqito/channel_ops/complementary_channel.py
+++ toqito/channel_ops/complementary_channel.py
@@ -16,6 +16,53 @@ def complementary_channel(kraus_ops: list[np.ndarray]) -> list[np.ndarray]:
     we define the complementary Kraus operators :math:`K_i^C` by stacking the rows of
     :math:`K_i` from all Kraus operators vertically.
 
+    Examples
+    ==========
+    Suppose the following Kraus operators define a quantum channel:
+    .. math::
+        K_1 = \frac{1}{\sqrt{2}} \begin{pmatrix}
+            1 & 0 \\
+            0 & 0
+        \end{pmatrix},
+        K_2 = \frac{1}{\sqrt{2}} \begin{pmatrix}
+            0 & 1 \\
+            0 & 0
+        \end{pmatrix},
+        K_3 = \frac{1}{\sqrt{2}} \begin{pmatrix}
+            0 & 0 \\
+            1 & 0
+        \end{pmatrix},
+        K_4 = \frac{1}{\sqrt{2}} \begin{pmatrix}
+            0 & 0 \\
+            0 & 1
+        \end{pmatrix}
+    To compute the Kraus operators for the complementary map, we rearrange the rows of these
+    Kraus operators as follows:
+    >>> import numpy as np
+    >>> kraus_ops_Phi = [
+    ...     np.array([[1, 0], [0, 0]]),
+    ...     np.array([[0, 1], [0, 0]]),
+    ...     np.array([[0, 0], [1, 0]]),
+    ...     np.array([[0, 0], [0, 1]])
+    ... ]
+    >>> comp_kraus_ops = complementary_map(kraus_ops_Phi)
+    >>> for i, op in enumerate(comp_kraus_ops):
+    ...     print(f"Kraus operator {i + 1}:\n{op}\n")
+    The output would be:
+    .. math::
+        K_1^C = \frac{1}{\sqrt{2}} \begin{pmatrix}
+            1 & 0 \\
+            0 & 1 \\
+            0 & 0 \\
+            0 & 0
+        \end{pmatrix},
+        K_2^C = \frac{1}{\sqrt{2}} \begin{pmatrix}
+            0 & 0 \\
+            0 & 0 \\
+            1 & 0 \\
+            0 & 1
+        \end{pmatrix}
+
     References
     ==========
     .. bibliography::
