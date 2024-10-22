c095194378bc3e8bca7fb170708c2cebee5a12fc
vprusso <vincentrusso1@gmail.com>
2024-10-22 13:13:06 -0400

    Updating vector_to_density_matrix function to just to_density_matrix.

--- toqito/matrix_ops/__init__.py
+++ toqito/matrix_ops/__init__.py
@@ -1,5 +1,6 @@
 """Operations on matrices and vectors."""
 
+from toqito.matrix_ops.to_density_matrix import to_density_matrix
 from toqito.matrix_ops.tensor import tensor
 from toqito.matrix_ops.vec import vec
 from toqito.matrix_ops.unvec import unvec
@@ -7,4 +8,3 @@ from toqito.matrix_ops.vectors_from_gram_matrix import vectors_from_gram_matrix
 from toqito.matrix_ops.vectors_to_gram_matrix import vectors_to_gram_matrix
 from toqito.matrix_ops.calculate_vector_matrix_dimension import calculate_vector_matrix_dimension
 from toqito.matrix_ops.tensor_comb import tensor_comb
-from toqito.matrix_ops.to_density_matrix import to_density_matrix
