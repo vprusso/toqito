467666a0ae1dcf4d95be8bceb51af9a246ba1a96
Shivansh Mittal <88429611+Shivansh20128@users.noreply.github.com>
2024-11-04 22:03:59 +0530

    Adding ability to perturb a vector by some factor (#861)
    * adding perturb vectors
    
    * update perturb vectors
    
    * editing module import
    
    * update import modules
    
    * support for no perturbations
    
    * adding docstring example
    
    * adding imports
    
    * update import order
    
    * removing output in example
    
    * update
    
    * update
    
    * removing comments
    
    * Update toqito/matrix_ops/perturb_vectors.py
    
    Co-authored-by: Purva Thakre <66048318+purva-thakre@users.noreply.github.com>
    
    * removing np.testing
    
    * update perturb_vectors
    
    * adding output manually
    
    ---------
    
    Co-authored-by: Purva Thakre <66048318+purva-thakre@users.noreply.github.com>

--- toqito/matrix_ops/__init__.py
+++ toqito/matrix_ops/__init__.py
@@ -7,3 +7,5 @@ from toqito.matrix_ops.vectors_from_gram_matrix import vectors_from_gram_matrix
 from toqito.matrix_ops.vectors_to_gram_matrix import vectors_to_gram_matrix
 from toqito.matrix_ops.calculate_vector_matrix_dimension import calculate_vector_matrix_dimension
 from toqito.matrix_ops.to_density_matrix import to_density_matrix
+from toqito.matrix_ops.perturb_vectors import perturb_vectors
+
---
+++ toqito/matrix_ops/perturb_vectors.py
@@ -0,0 +1,36 @@
+"""Perturb vectors is used to add a small random number to each element of a vector.
+
+A random value is added sampled from a normal distribution scaled by `eps`.
+"""
+
+import numpy as np
+
+
+def perturb_vectors(vectors: list[np.ndarray], eps: float = 0.1) -> list[np.ndarray]:
+    """Perturb the vectors by adding a small random number to each element.
+
+    :param vectors: List of vectors to perturb.
+    :param eps: Amount by which to perturb vectors.
+    :return: Resulting list of perturbed vectors by a factor of epsilon.
+
+    Example:
+    ==========
+
+        >>> from toqito.matrix_ops import perturb_vectors
+        >>> import numpy as np
+        >>> vectors = [np.array([1.0, 2.0]), np.array([3.0, 4.0])]
+        >>> perturb_vectors(vectors, eps=0.1) # doctest: +SKIP
+        array([[0.47687587, 0.87897065],
+               [0.58715549, 0.80947417]])
+
+    """
+    perturbed_vectors: list[np.ndarray] = []
+    for i, v in enumerate(vectors):
+        if eps == 0:
+            perturbed_vectors.append(v)
+        else:
+            perturbed_vectors.append(v + np.random.randn(v.shape[0]) * eps)
+
+            # Normalize the vectors after perturbing them.
+            perturbed_vectors[i] = perturbed_vectors[i] / np.linalg.norm(perturbed_vectors[i])
+    return np.array(perturbed_vectors)
