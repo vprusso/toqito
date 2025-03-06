d306f63d6886daaf49bdbd73fd32159160966534
harshvardhan-pandey <harshvardhan.pandey@research.iiit.ac.in>
2025-03-06 05:59:06 +0000

    Add is_pseudo_unitary

---
+++ toqito/matrix_props/tests/test_is_pseudo_unitary.py
@@ -0,0 +1,37 @@
+"""Test is_pseudo_unitary."""
+
+import numpy as np
+
+from toqito.matrix_props import is_pseudo_unitary
+from toqito.rand import random_unitary
+
+
+def test_is_unitary_random():
+    """Test that random unitary matrix with signature diag([1, 1]) return True."""
+    mat = random_unitary(2)
+    np.testing.assert_equal(is_pseudo_unitary(mat, p=2, q=0), True)
+
+
+def test_is_pseudo_unitary_lorentz_boost():
+    """Test that Lorentz boost matrix with signature diag([1, -1]) returns True."""
+    theta = np.random.rand()
+    mat = np.array([[np.cosh(theta), np.sinh(theta)], [np.sinh(theta), np.cosh(theta)]])
+    np.testing.assert_equal(is_pseudo_unitary(mat, p=1, q=1), True)
+
+
+def test_is_not_pseudo_unitary():
+    """Test that non pseudo unitary matrix returns False."""
+    mat = np.array([[1, 0], [1, -1]])
+    np.testing.assert_equal(is_pseudo_unitary(mat, p=1, q=1), False)
+
+
+def test_is_not_pseudo_unitary_incorrect_signatur_dimensions():
+    """Test that non-unitary matrix returns False."""
+    mat = np.array([[1, 0], [1, 1]])
+    np.testing.assert_equal(is_pseudo_unitary(mat, p=4, q=5), False)
+
+
+def test_is_pseudo_unitary_not_square():
+    """Input must be a square matrix."""
+    mat = np.array([[-1, 1, 1], [1, 2, 3]])
+    np.testing.assert_equal(is_pseudo_unitary(mat, p=1, q=1), False)
