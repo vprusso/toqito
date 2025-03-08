220b3273a25fb1a6830b15c383eedea93db41458
Sai Nandan Morapakula <sainandanm2002@gmail.com>
2025-03-07 23:21:04 -0500

    Update is_etf.py

--- toqito/matrix_props/is_etf.py
+++ toqito/matrix_props/is_etf.py
@@ -1,27 +1,23 @@
-'''Checks if matrix forms an equilangular tight frame (ETF).'''
+"""Checks if matrix forms an equilangular tight frame (ETF)."""
 import numpy as np
 from toqito.matrix_ops import vectors_to_gram_matrix
 
-def is_etf(mat: np.ndarray, rtol: float = 1e-05, atol: float = 1e-08) -> bool:
-    r"""Check if matrix constitutes an equilangular tight frame.
-    
-    Args:
-        mat(array): A matrix of any size
+
+def is_etf(mat: np.ndarray) -> bool:
+    r"""
+    Checks if a matrix constitutes an equilangular tight frame(ETF).
     
     Definition taken from:
-    :cite:`http://users.cms.caltech.edu/~jtropp/conf/Tro05-Complex-Equiangular-SPIE-preprint.pdf`.
+    :cite:`hoffman2011complexequiangulartightframes`.
     
-    A matrix A of any size constitutes an equilangular tight frame if it satisfies three conditions:
-        1) If each of the columns of the matrix has unit norm. 
-        2) If all the diagnol elements of the gram matrix is one and its off-diagnol elements are constant.
-        3) If A.A^* = (ncols / nrows). I(Identity matrix)
-    
-    where:
-        - A^* is the conjugate transpose (Hermitian transpose) of A.
+    A matrix \(A\) constitutes an equilangular tight frame if it satisfies three conditions:
+        1) If each of the columns of the matrix \(A\) has unit norm. 
+        2) If all the diagonol elements of the gram matrix is one and its off-diagonol elements are constant.
+        3) \(A A* = \frac{\text{ncols}}{\text{nrows}} \, I\), where \(A*\) is the conjugate transpose of \(A\).
     
     Examples
     ========
-    >>> # Example of a ETF matrix.
+    >>> # Example of an ETF matrix.
     >>> import numpy as np
     >>> from toqito.matrix_ops import vectors_to_gram_matrix
     >>> from toqito.matrix_props import is_etf
@@ -35,20 +31,18 @@ def is_etf(mat: np.ndarray, rtol: float = 1e-05, atol: float = 1e-08) -> bool:
         :filter: docname in docnames
         
     :param mat: The matrix to check.
-    :param rtol: The relative tolerance parameter (default 1e-05).
-    :param atol: The absolute tolerance parameter (default 1e-08).
     :return: Return :code:True if the matrix constitutes ETF, and :code:False otherwise.
     
     """
     nrows, ncols = mat.shape[0], mat.shape[1]
     
-    #Checks if the each column has a unit norm.
+    # Checks if the each column has a unit norm.
     for col in range(ncols):
         if not np.isclose(np.linalg.norm(mat[:,col]), 1):
             return False
 
         
-    #Checks if the matrix is equiangular
+    # Checks if the matrix is equiangular
     col_vectors = [mat[:, i] for i in range(mat.shape[1])]
     if not np.allclose(np.diag(vectors_to_gram_matrix(col_vectors)), 1):
         return False
