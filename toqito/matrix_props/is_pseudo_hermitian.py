3c23de4bfccb281641a662e72243877c349e7553
harshvardhan-pandey <harshvardhan.pandey@research.iiit.ac.in>
2025-03-12 05:16:53 +0000

    Added Docstring

--- toqito/matrix_props/is_pseudo_hermitian.py
+++ toqito/matrix_props/is_pseudo_hermitian.py
@@ -5,8 +5,74 @@ import numpy as np
 from toqito.matrix_props import has_same_dimension, is_hermitian, is_square
 
 
-def is_pseudo_unitary(mat: np.ndarray, signature: np.ndarray, rtol: float = 1e-05, atol: float = 1e-08) -> bool:
-    r"""Check if a matrix is pseudo-hermitian."""
+def is_pseudo_hermitian(mat: np.ndarray, signature: np.ndarray, rtol: float = 1e-05, atol: float = 1e-08) -> bool:
+    r"""Check if a matrix is pseudo-Hermitian.
+
+    A matrix :math:`H` is pseudo-Hermitian with respect to a given signature matrix :math:`\eta` if it satisfies:
+
+    .. math::
+        \eta H \eta^{-1} = H^{\dagger},
+
+    where:
+        - :math:`H^{\dagger}` is the conjugate transpose (Hermitian transpose) of :math:`H`,
+        - :math:`\eta` is a Hermitian, invertible matrix.
+
+    Examples
+    ==========
+
+    Consider the following matrix:
+
+    .. math::
+        H = \begin{pmatrix}
+            1 & 1+i \\
+            -1+i & -1
+        \end{pmatrix}
+
+    with the signature matrix:
+
+    .. math::
+        \eta = \begin{pmatrix}
+            1 & 0 \\
+            0 & -1
+        \end{pmatrix}
+
+    Our function confirms that :math:`H` is pseudo-Hermitian:
+
+    >>> import numpy as np
+    >>> from toqito.matrix_props import is_pseudo_hermitian
+    >>> H = np.array([[1, 1j], [-1j, 1]])
+    >>> eta = np.array([[1, 0], [0, -1]])
+    >>> is_pseudo_hermitian(H, eta)
+    True
+
+    However, the following matrix :math:`A`
+
+    .. math::
+        A = \begin{pmatrix}
+            1 & i \\
+            -i & 1
+        \end{pmatrix}
+
+    is not pseudo-Hermitian with respect to the same signature matrix:
+
+    >>> A = np.array([[1, 1j], [-1j, 1]])
+    >>> is_pseudo_hermitian(B, eta)
+    False
+
+    References
+    ==========
+
+    .. bibliography::
+        :filter: docname in docnames
+
+    :param mat: The matrix to check.
+    :param signature: The signature matrix :math:`\eta`, which must be Hermitian and invertible.
+    :param rtol: The relative tolerance parameter (default 1e-05).
+    :param atol: The absolute tolerance parameter (default 1e-08).
+    :raises ValueError: If `signature` is not Hermitian or not invertible.
+    :return: Return :code:`True` if the matrix is pseudo-Hermitian, and :code:`False` otherwise.
+
+    """
     if not is_hermitian(signature):
         raise ValueError("Signature not hermitian matrix.")
 
@@ -17,4 +83,4 @@ def is_pseudo_unitary(mat: np.ndarray, signature: np.ndarray, rtol: float = 1e-0
         return False
 
     eta_H_inv_eta = signature @ mat @ np.linalg.inv(signature)
-    return np.allclose(eta_H_inv_eta, mat.conj(), rtol=rtol, atol=atol)
+    return np.allclose(eta_H_inv_eta, mat.conj().T, rtol=rtol, atol=atol)
