677a9c8da9082e7f1f4c0051afc420adb1ab8f5f
Purva Thakre <purva.athakre@gmail.com>
2025-04-04 10:39:25 -0500

    test github workflow

--- docs/conf.py
+++ docs/conf.py
@@ -11,9 +11,9 @@
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 import os
 import sys
-# sys.path.insert(0, os.path.abspath('.'))
-# sys.path.insert(0, os.path.abspath('..'))
-# sys.path.insert(0, os.path.abspath('../..'))
+sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('../..'))
 
 # -- Project information -----------------------------------------------------
 
@@ -44,6 +44,7 @@ extensions = [
     "sphinx.ext.napoleon",
     "sphinxcontrib.bibtex",
     "sphinx_copybutton",
+    "jupyter_sphinx",
 ]
 
 bibtex_bibfiles = ["refs.bib"]
