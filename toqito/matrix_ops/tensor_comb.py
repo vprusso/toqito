095dbe671f1823316e69bd2c63e0944c73416c01
deba-cyber <64672586+deba-cyber@users.noreply.github.com>
2025-04-11 18:06:16 +0530

    Add files via upload

--- toqito/matrix_ops/tensor_comb.py
+++ toqito/matrix_ops/tensor_comb.py
@@ -7,9 +7,11 @@ import numpy as np
 from toqito.matrix_ops import to_density_matrix
 
 
-def tensor_comb(k: int, states: list[np.ndarray],
-                mode: str,
-                density_matrix: bool) -> dict[tuple[int, ...], np.ndarray]:
+def tensor_comb(
+    k: int,
+    states: list[np.ndarray],
+    mode: str, density_matrix: bool
+    ) -> dict[tuple[int, ...], np.ndarray]:
     r"""Generate all possible tensor product combinations of quantum states (vectors).
 
     This function creates a tensor product of quantum state vectors by generating all possible sequences of length `k`
@@ -55,29 +57,28 @@ def tensor_comb(k: int, states: list[np.ndarray],
         :filter: docname in docnames
 
     :raises ValueError: If the input list of states is empty.
-    Args:
-        k: The length of the sequence.
-        states: A list of state vectors.
-        mode: Determines the type of sequences.
+    :param k: The length of the sequence.
+    :param states: A list of state vectors.
+    :mode: Determines the type of sequences.
             - "non-injective": Allows repetitions in sequences.
-            - "injective": Ensures sequences are injective (no repetitions). 
-            - "diagonal": Only sequences with repeated indices (diagonal elements). 
+            - "injective": Ensures sequences are injective (no repetitions).
+            - "diagonal": Only sequences with repeated indices (diagonal elements).
+    :density_matrix: Determines whether the return is density matrix or ket.
 
-    Returns:
-        A dictionary where keys are tuples representing sequences of state indices,
-        and values are density matrices of the tensor products of the corresponding  
-        state vectors.
+    :return: A dictionary where keys are tuples representing sequences of state indices,
+        and values are density matrices of the tensor products of the corresponding
+        state vectors or tensor products of the corresponding state vectors based on
+        input density_matrix = True / False.
+
     """
     if not states:
         raise ValueError("Input list of states cannot be empty.")
 
     if mode not in ("injective", "non-injective", "diagonal"):
-        raise ValueError(
-            "`mode` must be 'injective','non-injective', or 'diagonal'.")
+        raise ValueError("`mode` must be 'injective','non-injective', or 'diagonal'.")
 
     if mode == "injective" and k > len(states):
-        raise ValueError(
-            "k must be less than or equal to the number of states for injective sequences.")
+        raise ValueError("k must be less than or equal to the number of states for injective sequences.")
 
     # Generate sequences based on the selected mode
     match mode:
@@ -86,7 +87,7 @@ def tensor_comb(k: int, states: list[np.ndarray],
         case "non-injective":
             sequences = list(itertools.product(range(len(states)), repeat=k))
         case "diagonal":
-            sequences = [(i,)*k for i in range(len(states))]
+            sequences = [(i,) * k for i in range(len(states))]
 
     sequences_of_states = {}
     for seq in sequences:
@@ -96,8 +97,7 @@ def tensor_comb(k: int, states: list[np.ndarray],
             sequence_tensor_product = np.kron(sequence_tensor_product, state)
 
         if density_matrix:
-            sequences_of_states[seq] = to_density_matrix(
-                sequence_tensor_product)
+            sequences_of_states[seq] = to_density_matrix(sequence_tensor_product)
         else:
             sequences_of_states[seq] = sequence_tensor_product
 
