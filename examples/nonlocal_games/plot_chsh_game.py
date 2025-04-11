3f98ec7ccb8964b46c9c6418a39958290e162228
shivasai <139239760+jajapuramshivasai@users.noreply.github.com>
2025-04-11 16:21:44 +0530

    ,,

--- examples/nonlocal_games/plot_chsh_game.py
+++ examples/nonlocal_games/plot_chsh_game.py
@@ -1,4 +1,6 @@
+# ruff: noqa: D205, D400, D415
 """
+
 CHSH Game Example
 =================
 
@@ -6,6 +8,8 @@ This example calculates the classical and quantum values for the CHSH game
 using the toqito library. It demonstrates how to construct the necessary matrices
 and use the XORGame function.
 """
+
+
 
 
 # sphinx_gallery_thumbnail_number = 1
