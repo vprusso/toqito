36cd610c253529c50644376c8a6a3997a8929f00
shivasai <139239760+jajapuramshivasai@users.noreply.github.com>
2025-04-11 17:06:37 +0530

    updated config

--- docs/conf.py
+++ docs/conf.py
@@ -56,15 +56,7 @@ sphinx_gallery_conf = {
     "remove_config_comments": True,
 }
 
-
 suppress_warnings = ["toc.not_included", "toc.not_readable"]
-
-
-linkcheck_ignore = [
-    "https://uwspace.uwaterloo.ca/handle/10012/9572",  # Temporarily ignore until fixed
-]
-
-
 bibtex_bibfiles = ["refs.bib"]
 bibtex_default_style = "unsrt"
 suppress_warnings = ["bibtex.duplicate_label", "bibtex.duplicate_citation"]
