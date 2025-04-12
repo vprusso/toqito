dc80b4ca38b655a2e3286251b7f96d304fba5fea
vprusso <vincentrusso1@gmail.com>
2025-04-12 12:59:18 -0400

    lint: fixing linter

--- toqito/matrices/__init__.py
+++ toqito/matrices/__init__.py
@@ -3,6 +3,7 @@
 from toqito.matrices.gen_pauli_x import gen_pauli_x
 from toqito.matrices.gen_pauli_z import gen_pauli_z
 from toqito.matrices.cnot import cnot
+from toqito.matrices.comparison import comparison
 from toqito.matrices.fourier import fourier
 from toqito.matrices.gell_mann import gell_mann
 from toqito.matrices.gen_gell_mann import gen_gell_mann
@@ -11,4 +12,3 @@ from toqito.matrices.hadamard import hadamard
 from toqito.matrices.pauli import pauli
 from toqito.matrices.standard_basis import standard_basis
 from toqito.matrices.cyclic_permutation_matrix import cyclic_permutation_matrix
-from toqito.matrices.comparison import comparison
