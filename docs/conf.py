adb876e29ec1282223d3f60d3733185702e1a886
shivasai <139239760+jajapuramshivasai@users.noreply.github.com>
2025-04-13 02:30:46 +0530

    fixed code style

--- docs/conf.py
+++ docs/conf.py
@@ -52,9 +52,9 @@ extensions = [
 
 sphinx_gallery_conf = {
     "examples_dirs": "../examples",  # Path to example scripts
-    "gallery_dirs": "auto_examples", # Output directory for generated example galleries
-    "filename_pattern": "/plot_",    # Regex to filter example files by name i.e those starting with 'plot_'
-    "run_stale_examples": True,      # Re-run examples that are outdated compared to source code
+    "gallery_dirs": "auto_examples",  # Output directory for generated example galleries
+    "filename_pattern": "/plot_",  # Regex to filter example files by name i.e those starting with 'plot_'
+    "run_stale_examples": True,  # Re-run examples that are outdated compared to source code
     "remove_config_comments": True,  # Strip configuration comments from example scripts in the gallery
 }
 
