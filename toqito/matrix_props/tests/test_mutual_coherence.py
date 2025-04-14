62c70c53efe530b8d9a5c61fff76ef8304389399
dvdllrch <dvdllrch@gmail.com>
2025-04-14 14:40:23 +0200

    correction of test examples expected  result

--- toqito/matrix_props/tests/test_mutual_coherence.py
+++ toqito/matrix_props/tests/test_mutual_coherence.py
@@ -16,7 +16,7 @@ from toqito.matrix_props.mutual_coherence import mutual_coherence
         # Test case 3:
         (np.array([[1, 0], [1, 1]]), 0.7071067811865475),  # sqrt(2)/2
         # Test case 4:
-        (np.array([[1, 0, 1], [0, 1, 1], [1, 1, 0]]), 1 / 3),
+        (np.array([[1, 0, 1], [0, 1, 1], [1, 1, 0]]), 1 / 2),
     ],
 )
 def test_mutual_coherence(matrix, expected_coherence):
