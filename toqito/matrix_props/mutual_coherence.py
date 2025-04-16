4960997cb0321a2ddbca7afd37c6d048402767d5
dvdllrch <dvdllrch@gmail.com>
2025-04-16 21:25:30 +0200

    doc change

--- toqito/matrix_props/mutual_coherence.py
+++ toqito/matrix_props/mutual_coherence.py
@@ -8,7 +8,7 @@ def mutual_coherence(setofvecs: list[np.ndarray]) -> float:
 
     The mutual coherence of a set of vectors is defined as the maximum absolute
     value of the inner product between any two distinct vectors, divided by the
-    product of their norms :cite:WikiMutualCoh. It provides a measure of how
+    product of their norms :cite:`WikiMutualCoh`. It provides a measure of how
     similar the vectors are to each other.
 
     Examples
