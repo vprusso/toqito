45bfc45888a5c2ef107de7f39bb47e5618e971bc
deba-cyber <64672586+deba-cyber@users.noreply.github.com>
2025-04-18 11:36:55 +0530

    Add files via upload

--- toqito/matrix_ops/tests/test_tensor_comb.py
+++ toqito/matrix_ops/tests/test_tensor_comb.py
@@ -1,4 +1,4 @@
-"""Test tensor_comb for different cases."""
+"""Test tensor_comb."""
 import numpy as np
 import pytest
 
@@ -6,61 +6,49 @@ from toqito.matrix_ops import tensor_comb, to_density_matrix
 
 
 @pytest.mark.parametrize(
-    "k, states, mode, density_matrix, expected_keys, expected_error",
+    "states, k, mode, density_matrix, expected_keys, expected_error",
     [
-        (3,
         # Injective (default).
-         [np.array([1, 0]), np.array([0, 1])],
-         "injective", False,
+        ([np.array([1, 0]), np.array([0, 1])],
+         3, "injective", False,
          None,
          ValueError),
         # Invalid mode
-        (2,
-         [np.array([1, 0]), np.array([0, 1])],
-         "invalid", False,
+        ([np.array([1, 0]), np.array([0, 1])],
+         2, "invalid", False,
          None,
          ValueError),
-        # Non-injective mode with k = 1
-        (1,
-         [np.array([1, 0]), np.array([0, 1])],
-         "non-injective", False,
-         [(0,), (1,)],
-         None),
         # Injective mode with k = 1
-        (1,
-         [np.array([1, 0]), np.array([0, 1])],
-         "injective", False,
+        ([np.array([1, 0]), np.array([0, 1])],
+         1, "injective", False,
          [(0,), (1,)],
          None),
         # Non-injective mode with k = 1
-        (1,
-         [np.array([1, 0]), np.array([0, 1])],
-         "injective", False,
+        ([np.array([1, 0]), np.array([0, 1])],
+         1, "non-injective", False,
          [(0,), (1,)],
          None),
         # Non-injective mode with k = 2
-        (2,
-         [np.array([1, 0]), np.array([0, 1])],
-         "non-injective", False,
+        ([np.array([1, 0]), np.array([0, 1])],
+         2, "non-injective", False,
          [(0, 0), (0, 1), (1, 0), (1, 1)],
          None),
          # Non-injective mode with k = 3
-        (3,
-         [np.array([1, 0]), np.array([0, 1])],
-         "non-injective", False,
-         [(0, 0, 0), (0, 0, 1)],
-         None),
+        ([np.array([1, 0]), np.array([0, 1])],
+         3, "non-injective", False,
+         [(0, 0, 0), (0, 0, 1)],	
          # other possible sequences omitted for brevity
+        None),
     ],
 )
-def test_tensor_comb(k, states, mode, density_matrix, expected_keys, expected_error):
+def test_tensor_comb(states, k, mode, density_matrix, expected_keys, expected_error):
     """Test the tensor_comb function."""
     if expected_error:
         with pytest.raises(expected_error):
-            tensor_comb(k, states, mode=mode, density_matrix=density_matrix)
+            tensor_comb(states, k, mode=mode, density_matrix=density_matrix)
 
     else:
-        result = tensor_comb(k, states, mode=mode,
+        result = tensor_comb(states, k, mode=mode,
                              density_matrix=density_matrix)
         result_keys = list(result.keys())
 
