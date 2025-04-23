fc1c4633c33f6a14ea59df761ba0ca4afef966d6
karthik-nv <karthik.nv611@gmail.com>
2025-04-24 02:36:49 +0530

    ref fixes. restructured tests

--- toqito/matrices/tests/test_operator_sinkhorn.py
+++ toqito/matrices/tests/test_operator_sinkhorn.py
@@ -24,7 +24,7 @@ def test_operator_sinkhorn_unitary_invariance():
 
 def test_operator_sinkhorn_trace_property():
     """Test the trace property of operator Sinkhorn when a valid inputs are passed."""
-    rho = random_density_matrix(9)  # 9-dimensional density matrix.
+    rho = random_density_matrix(9)
 
     # The dimension `3` divides evenly into `9`, so no error should occur.
     sigma, local_ops = operator_sinkhorn(rho, dim=[3])
@@ -35,21 +35,17 @@ def test_operator_sinkhorn_trace_property():
 
 # Test partial traces for bipartite and tripartite systems.
 @pytest.mark.parametrize(
-    "test_input, expected",
+    "rho1, dim1, expected_identity, target_subsys",
     [
-        # Random bipartite system with (3x3 qubit, 9 dimensional)
-        ([random_density_matrix(9), [3, 3]], [np.eye(3) * (1 / 3), 0]),
+        # Random bipartite system with (3x3 qubit, 9 dimensional).
+        (random_density_matrix(9), [3, 3], np.eye(3) * (1 / 3), 0),
         # Random tripartite system with (2x2x2 qubit, 8 dimensional).
-        ([random_density_matrix(8), [2, 2, 2]], [np.eye(2) * (1 / 2), [0, 2]]),
+        (random_density_matrix(8), [2, 2, 2], np.eye(2) * (1 / 2), [0, 2]),
     ],
 )
-def test_operator_sinkhorn_partial_trace(test_input, expected):
+def test_operator_sinkhorn_partial_trace(rho1, dim1, expected_identity, target_subsys):
     """Test partial traces for bipartite and tripartite systems."""
-    rho1, dim1 = test_input
     sigma, local_ops = operator_sinkhorn(rho=rho1, dim=dim1)
-
-    # Expected partial trace should be proportional to identity matrix.
-    expected_identity, target_subsys = expected
 
     # Partial trace on the first subsystem.
     pt = partial_trace(sigma, target_subsys, dim=dim1)
@@ -61,11 +57,14 @@ def test_operator_sinkhorn_partial_trace(test_input, expected):
 
 # Test error handling in different scenarios.
 @pytest.mark.parametrize(
-    "test_input,expected_msg",
+    "rho1, dim1, iters, error_type, expected_msg",
     [
         # Singular matrix.
         (
-            [np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]), [2, 2]],
+            np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]),
+            [2, 2],
+            10_000,
+            ValueError,
             (
                 "The operator Sinkhorn iteration does not converge for rho. "
                 "This is often the case if rho is not of full rank."
@@ -73,7 +72,10 @@ def test_operator_sinkhorn_partial_trace(test_input, expected):
         ),
         # Invalid dim array with size 1.
         (
-            [random_density_matrix(8), [3]],
+            random_density_matrix(8),
+            [3],
+            10_000,
+            ValueError,
             (
                 "If dim is of size 1, rho must be square and dim[0] must evenly divide rho.shape[0]; "
                 "please provide the dim array containing the dimensions of the subsystems."
@@ -81,32 +83,29 @@ def test_operator_sinkhorn_partial_trace(test_input, expected):
         ),
         # Invalid dim array - when product of the dim array != density matrix dims.
         (
-            [random_density_matrix(8), [4, 3, 2]],  # Since 4*3*2 != 8.
+            random_density_matrix(8),
+            [4, 3, 2],  # Since 4*3*2 != 8.
+            10_000,
+            ValueError,
             "Product of dimensions [4, 3, 2] does not match rho dimension 8.",
         ),
         # Non square matrix input.
-        ([np.random.rand(4, 5), [2, 2]], "Input 'rho' must be a square matrix."),
+        (np.random.rand(4, 5), [2, 2], 10_000, ValueError, "Input 'rho' must be a square matrix."),
+        # Insufficient iteration limit.
+        (
+            random_density_matrix(4, seed=42),
+            [2, 2],
+            20,
+            RuntimeError,
+            "operator_sinkhorn did not converge within 20 iterations.",
+        ),
     ],
 )
-def test_operator_sinkhorn_errors(test_input, expected_msg):
+def test_operator_sinkhorn_errors(rho1, dim1, iters, error_type, expected_msg):
     """Test error handling in different scenarios."""
-    # Unpack test inputs.
-    rho, dim1 = test_input
-
     try:
-        operator_sinkhorn(rho, dim=dim1)
-    except ValueError as e:
-        assert str(e) == expected_msg
-
-
-def test_operator_sinkhorn_max_iterations():
-    """Test operator sinkhorn on insufficient iteration limit."""
-    rho_random = random_density_matrix(4, seed=42)
-    num_iters = 20
-    try:
-        operator_sinkhorn(rho=rho_random, dim=[2, 2], max_iterations=num_iters)
-    except RuntimeError as e:
-        expected_msg = f"operator_sinkhorn did not converge within {num_iters} iterations."
+        operator_sinkhorn(rho=rho1, dim=dim1, max_iterations=iters)
+    except error_type as e:
         assert str(e) == expected_msg
 
 
