cd9d5a5fbdfc2dbbec4210d2501e5d04cf025b82
Vincent Russo <vincentrusso1@gmail.com>
2025-05-03 12:52:49 -0400

    Update toqito/matrix_props/is_k_incoherent.py
    Co-authored-by: Purva Thakre <66048318+purva-thakre@users.noreply.github.com>

--- toqito/matrix_props/is_k_incoherent.py
+++ toqito/matrix_props/is_k_incoherent.py
@@ -42,8 +42,8 @@ def is_k_incoherent(mat: np.ndarray, k: int, tol: float = 1e-15) -> bool:
 
     See Also
     ========
-    :func:`.toqito.state_props.is_antidistinguishable`
-    :func:`.toqito.matrix_props.is_absolutely_k_incoherent`
+    :func:`.is_antidistinguishable`
+    :func:`.is_absolutely_k_incoherent`
 
     References
     ==========
