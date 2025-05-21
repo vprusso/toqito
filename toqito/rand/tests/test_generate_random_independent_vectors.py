7796f3fbd4a4b57a758ff30f9eddf690defbe51a
Elizabetht1 <eterveen@andrew.cmu.edu>
2025-05-21 18:04:15 -0500

    updated unit tests

--- toqito/rand/tests/test_generate_random_independent_vectors.py
+++ toqito/rand/tests/test_generate_random_independent_vectors.py
@@ -2,55 +2,50 @@
 
 import numpy as np
 import pytest
-from numpy.testing import assert_allclose, assert_equal, assert_raises
+from numpy.testing import assert_equal, assert_raises
 
 from toqito.rand.generate_random_independent_vectors import generate_random_independent_vectors
-from toqito.matrix_props.is_linearly_independent import is_linearly_independent 
 
 
 @pytest.mark.parametrize(
-    "n,m",
+    "num_vecs,dim",
     [
         # Test with a matrix of dimension 2.
-        (2,2),
+        (2, 2),
         # Test with a matrix of higher dimension.
-        (4,4),
+        (4, 4),
         # Test with yet another higher dimension.
-        (10,10),
+        (10, 10),
         # Test with yet another higher dimension.
-        (2,5),
-        (2,10),
+        (2, 5),
+        (2, 10),
     ],
 )
+def test_generate_random_independent_vectors(num_vecs, dim):
+    """Test for generate_random_independent_vectors function."""
+    linear_indep = generate_random_independent_vectors(num_vecs, dim)
 
-def test_random_circulant_gram_matrix(n,m):
-    linear_indep = generate_random_independent_vectors(n,m)
-    
-    
-    # verify the matrix has the correct dimensions 
-    assert_equal(linear_indep.shape, (n, m))
+    # verify the matrix has the correct dimensions
+    assert_equal(linear_indep.shape, (dim, num_vecs))
 
-
-    # verify the matrix is real 
+    # verify the matrix is real
     assert_equal(np.isreal(linear_indep).all(), True)
 
-    # verify the vectors are linearaly independent 
-    # by checking that the corresponding matrix is invertible,
-    # which is iff w the determinant being non-zero 
-    assert_equal(np.linalg.det(linear_indep) == 0, False)
+    # verify the vectors are linearaly independent
+    # by confirming the rank of the vector space
+    # is equivalent to the number of vectors generated
+    assert_equal(np.linalg.matrix_rank(linear_indep) == num_vecs, True)
 
 
 @pytest.mark.parametrize(
     "n,m",
     [
         # Test with a matrix of dimension 2.
-        (3,2),
+        (3, 2),
         # Test with a matrix of higher dimension.
-        (20,15),
+        (20, 15),
     ],
 )
-
-def test_random_circulant_gram_matrix_failure(n,m):
-    assert_raises(ValueError,generate_random_independent_vectors,n,m)
-    
-
+def test_generate_random_independent_vectors_failure(n, m):
+    """Test for generate_random_independent_vectors function; should fail."""
+    assert_raises(ValueError, generate_random_independent_vectors, n, m)
