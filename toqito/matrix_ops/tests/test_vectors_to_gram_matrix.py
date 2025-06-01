9d6bd70c362d3344d1385e86b4c2bb6d3622314e
poig <laolianglaoliang@gmail.com>
2025-06-01 23:57:12 +1000

    fix vec import issue

--- toqito/matrix_ops/tests/test_vectors_to_gram_matrix.py
+++ toqito/matrix_ops/tests/test_vectors_to_gram_matrix.py
@@ -3,7 +3,7 @@
 import numpy as np
 import pytest
 
-from toqito.matrix_ops import vectors_to_gram_matrix
+from toqito.perms import vectors_to_gram_matrix
 from toqito.states import trine
 
 e_0, e_1 = np.array([[1], [0]]), np.array([[0], [1]])
