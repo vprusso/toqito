204dacdef2ed08d69fb63d5bc6774ffcf3b73148
unknown <contact.mainakroy@gmail.com>
2025-06-11 02:03:24 +0530

    draft implementation

---
+++ toqito/state_props/abs_ppt_constraints.py
@@ -0,0 +1,98 @@
+"""Compute the constraints on a spectrum for it to be absolutely PPT."""
+
+import numpy as np
+
+def abs_ppt_constraints(eigs: np.ndarray, p: int, max_constraints: int) -> list[np.ndarray]:
+    r"""Return the constraint matrices for the spectrum given by :code:`eigs` to be absolutely PPT.
+
+        This function is adapted from QETLAB.
+
+        Examples
+        ==========
+        Demonstrate how the function works with expected output.
+
+        .. jupyter-execute::
+
+            import numpy as np
+            x = np.array([[1, 2], [3, 4]])
+            print(x)
+
+        References
+        ==========
+        .. bibliography::
+            :filter: docname in docnames
+
+        :param eigs: A list of eigenvalues in non-increasing order.
+        :param p: The dimension of the smaller subsystem in the bipartite system.
+        :param max_constraints: The maximum number of constraint matrices to compute.
+        :return: A list of `max_constraints` constraint matrices which must be positive
+                 semidefinite for an absolutely PPT spectrum.
+
+    """
+    p_p = p * (p + 1) // 2
+    X = (p_p + 1) * np.ones((p, p), dtype=np.int32)
+    num_pool = np.ones((p_p, 1), dtype=np.int32)
+    constraints = []
+
+    # Hard-code matrices for p = 1, 2
+    if p == 1:
+        return []
+    if p == 2:
+        return [np.array([[2 * eigs[-1], eigs[-2] - eigs[0]],
+                          [eigs[-2] - eigs[0], 2 * eigs[-3]]])]
+
+    X[0, 0] = 1
+    X[0, 1] = 2
+    X[-1, -1] = p_p
+    X[-2, -1] = p_p - 1
+
+    def _fill_matrix(i: int, j: int, l_lim: int) -> None:
+        r"""Construct all valid orderings by backtracking."""
+        if len(constraints) == max_constraints:
+            return
+        j_p = j * (j + 1) // 2
+        u_lim = min(i * (p - j) + j_p + 1, p_p - 2)
+        for k in range(l_lim, u_lim + 1):
+            if num_pool[k] == 1:
+                X[i, j] = k
+                num_pool[k] = 0
+                if i == 0 or X[i - 1, j] < X[i, j]:
+                    if i == p - 2 and j == p - 2:
+                        if _check_cross(X, p) and _check_crossB(X, p):
+                            constraints.append(_create_constraint(eigs, X, p))
+                    elif j == p - 1:
+                        _fill_matrix(i + 1, i + 1, 3)
+                    else:
+                        _fill_matrix(i, j + 1, k + 1)
+                    # If still invalid, then exit?
+                num_pool[k] = 1
+        X[i, j] = p_p + 1
+
+    _fill_matrix(0, 2, 3)
+
+    return constraints
+
+def _check_cross(X: np.ndarray, p: int) -> bool:
+    r"""Checks that no "criss-cross" suborderings exist."""
+    for i in range(p):
+        for j in range(p):
+            for k in range(p):
+                for l in range(p):
+                    for m in range(p):
+                        for n in range(p):
+                            if (X[min(i, j), max(i, j)] > X[min(k, l), max(k, l)]
+                            and X[min(l, n), max(l, n)] > X[min(j, m), max(j, m)]
+                            and X[min(k, m), max(k, m)] > X[min(i, n), max(i, n)]):
+                                return False
+    return True
+
+def _create_constraint(eigs: np.ndarray, X: np.ndarray, p: int) -> np.ndarray:
+    r"""Return constraint matrix from order matrix."""
+    L = np.zeros((p, p))
+     # Set upper triangle + diagonal
+    upper_inds = np.triu_indices(p)
+    L[upper_inds] = eigs[-X[upper_inds]]
+    strictly_upper_inds = np.triu_indices(p, 1)
+    # Set lower triangle
+    L.T[strictly_upper_inds] = -eigs[np.unique(X[strictly_upper_inds], return_inverse=True)[1]]
+    return L + L.T
---
+++ toqito/state_props/is_abs_ppt.py
@@ -0,0 +1,85 @@
+"""Checks if a quantum state is absolutely PPT."""
+
+import numpy as np
+
+from toqito.matrix_props import is_square, is_positive_semidefinite
+from toqito.state_props import in_separable_ball, abs_ppt_constraints
+
+
+def is_abs_ppt(mat: np.ndarray, dim: int | list[int] = None) -> int:
+    r"""Determine whether or not a matrix is absolutely PPT :cite:`ref`.
+
+        This function is adapted from QETLAB.
+
+        Examples
+        ==========
+        Demonstrate how the function works with expected output.
+
+        .. jupyter-execute::
+
+            import numpy as np
+            x = np.array([[1, 2], [3, 4]])
+            print(x)
+
+        References
+        ==========
+        .. bibliography::
+            :filter: docname in docnames
+
+        :param mat: A square matrix.
+        :param dim: A 1-by-2 vector containing the dimensions of the
+                    subsystems on which :code:`mat` acts.
+        :return: :code:`1` if :code:`mat` is absolutely PPT,
+        :return: :code:`0` if :code:`mat is not absolutely PPT,
+        :return: :code:`-1` if the function could not decide.
+
+    """
+    if not is_square(mat):
+        raise ValueError("Matrix must be square")
+
+    nm = mat.shape[0]
+
+    if dim is None:
+        # Find the largest divisor d of nm such that d ** 2 <= nm
+        # nm won't be too large, so let's just use a for-loop
+        # Floating-point arithmetic is risky
+        d = 1
+        for j in range(1, nm + 1):
+            if j ** 2 > nm:
+                break
+            if nm % j == 0:
+                d = j
+        dim = [j, nm // j]
+
+    n, m = dim
+    p = min(n, m)
+    if not n * m == nm:
+        raise ValueError("Product of dim[0] and dim[1] must equal dimensions of mat")
+
+    # Quick checks
+    # 1. Is Hermitian
+    if not is_hermitian(mat):
+        return False
+    # Compute eigenvalues (in descending order)
+    eigs = np.linalg.eigvalsh(mat)[::-1]
+    # 2. Is PSD (TODO: use tolerances)
+    if eigs[-1] < 0:
+        return 0
+    # 3. Check if mat is in separable ball
+    if in_separable_ball(mat):
+        return 1
+    # 4. Check Theorem 7.2 of arXiv:1406.1277
+    if sum(eigs[:p-1]) <= eigs[-1] + sum(eigs[-p:]):
+        return 1
+
+    # Main check
+    # 2612 is the upper bound on the number of constraint matrices that is required
+    # for p = 6 using the QETLAB routine AbsPPTConstraints
+    # 2608 is the minimum value: The list of optimal constraint counts can be found
+    # in https://oeis.org/A237749
+    constraints = abs_ppt_constraints(eigs, p, 2612)
+    for constraint in constraints:
+        if not is_positive_semidefinite(constraint):
+            return 0
+    # We checked all constraints for p <= 6, but not for p >= 7
+    return 1 if p <= 6 else -1
