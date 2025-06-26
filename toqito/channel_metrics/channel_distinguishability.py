259babafca6caa4658d3692bfe28c9e074787dec
nemo <deba.bhat.90@gmail.com>
2025-06-26 13:58:16 +0530

    added minimax discrimination

--- toqito/channel_metrics/channel_distinguishability.py
+++ toqito/channel_metrics/channel_distinguishability.py
@@ -1,6 +1,7 @@
 """Computes the maximum probability of distinguishing two quantum channels."""
 
 import numpy as np
+import picos as pc
 
 from toqito.channel_metrics import completely_bounded_trace_norm
 from toqito.channel_ops import kraus_to_choi
@@ -10,19 +11,25 @@ from toqito.helper import channel_dim
 def channel_distinguishability(
     phi: np.ndarray | list[np.ndarray] | list[list[np.ndarray]],
     psi: np.ndarray | list[np.ndarray] | list[list[np.ndarray]],
-    p: list[float],
+    p: list[float] | None,
     dim: int | list[int] | np.ndarray = None,
+    strategy: str = "bayesian",
+    solver: str = "cvxopt",
+    primal_dual: str = "dual",
+    **kwargs,
 ) -> float:
     r"""Compute the optimal probability of distinguishing two quantum channels.
 
-    Bayesian discrimination of two quantum channels is implemented where a priori
-    probabilities are provided. (Section 3.3.3 of :footcite:`Watrous_2018_TQI`).
-    Implementation in QETLAB :footcite: `QETLAB_link` is used.
+        Bayesian and minimax discrimination of two quantum channels are implemented.
+        For Bayesian discrimination a priori probabilities are provided.
+        (Section 3.3.3 of :footcite:`Watrous_2018_TQI`).
+        Implementation in QETLAB :footcite: `QETLAB_link` is used for Bayesian
+        and Implementation in QuTIpy: footcite: `QuTIpy_link` for minimax is used.
 
 
     Examples
     ========
-    Compute the optimal probability of distinguishing two quantum channels:
+    Optimal probability of distinguishing two amplitude damping channels in the Bayesian setting:
 
     .. jupyter-execute::
 
@@ -36,6 +43,20 @@ def channel_distinguishability(
         p = [0.5, 0.5]
 
         channel_distinguishability(choi_ch_1, choi_ch_2, p)
+
+    Optimal probability of distinguishing two amplitude damping channels in the minimax setting:
+
+    .. jupyter-execute::
+
+        from toqito.channels import amplitude_damping
+        from toqito.channel_ops import kraus_to_choi
+        from toqito.channel_metrics import channel_distinguishability
+        # Define two amplitude damping channels with gamma=0.25 and gamma=0.5
+        choi_ch_1 = kraus_to_choi(amplitude_damping(gamma=0.25))
+        choi_ch_2 = kraus_to_choi(amplitude_damping(gamma=0.5))
+
+        channel_distinguishability(choi_ch_1, choi_ch_2, None, [2, 2], strategy="minimax",
+                        primal_dual="primal")
 
 
     References
@@ -53,6 +74,11 @@ def channel_distinguishability(
                 or as a (1d or 2d) list of numpy arrays whose entries are its Kraus operators.
     :param p: Prior probabilities of the two channels.
     :param dim: Input and output dimensions of the channels.
+    :param strategy: Whether to perform Bayesian or minimax discrimination task. Possible
+                                    values are "Bayesian" and "minimax". Defualt option is `strategy="Bayesian"`.
+    :param solver: Optimization option for `picos` solver. Default option is `solver="cvxopt"`.
+    :param primal_dual: Option for the optimization problem. Defualt option is `solver="cvxopt"`.
+    :param kwargs: Additional arguments to pass to picos' solve method.
     :return: The optimal probability of discriminating two quantum channels.
 
     """
@@ -68,19 +94,89 @@ def channel_distinguishability(
     if isinstance(psi, list):
         psi = kraus_to_choi(psi)
 
-    dim_phi, dim_psi = (d_in_phi, d_out_phi), (d_in_psi, d_out_psi)
+    dim_phi, dim_psi = np.array([d_in_phi, d_out_phi]), np.array([d_in_psi, d_out_psi])
 
     # checking for errors.
-    if dim_phi != dim_psi:
+    if not np.array_equal(dim_phi, dim_psi):
         raise ValueError("The channels must have the same dimension input and output spaces as each other.")
 
-    if len(p) != 2:
-        raise ValueError("p must be a probability distribution with 2 entries.")
+    if strategy == "bayesian":
+        if len(p) != 2:
+            raise ValueError("p must be a probability distribution with 2 entries.")
 
-    if max(p) >= 1:
-        return 1
+        if max(p) >= 1:
+            return 1
 
-    if (abs(sum(p) - 1)) != 0:
-        raise ValueError("Sum of prior probabilities must add up to 1.")
+        if (abs(sum(p) - 1)) != 0:
+            raise ValueError("Sum of prior probabilities must add up to 1.")
 
-    return 1 / 2 * (1 + completely_bounded_trace_norm(p[0] * phi - p[1] * psi))
+        return 1 / 2 * (1 + completely_bounded_trace_norm(p[0] * phi - p[1] * psi))
+
+    elif strategy == "minimax":
+        if primal_dual == "primal":
+            return _minimax_primal(phi, psi, d_in_phi[0], d_out_phi[0], solver=solver, **kwargs)
+        return _minimax_dual(phi, psi, d_in_phi[0], d_out_phi[0], solver=solver, **kwargs)
+
+
+def _minimax_dual(
+    phi: np.ndarray | list[np.ndarray] | list[list[np.ndarray]],
+    psi: np.ndarray | list[np.ndarray] | list[list[np.ndarray]],
+    dimA: int,
+    dimB: int,
+    primal_dual: str = "dual",
+    solver: str = "cvxopt",
+    **kwargs,
+) -> float:
+    """Find the dual problem for minimax quantum channel distinguishability SDP."""
+    J = list([phi, psi])
+
+    problem = pc.Problem()
+
+    a = pc.RealVariable("a", lower=0)
+    P = pc.RealVariable("P", 2)
+    Y = pc.HermitianVariable("Y", (dimA * dimB, dimA * dimB))
+
+    Y0 = pc.partial_trace(Y, 1)
+
+    problem.add_list_of_constraints(Y >> P[i] * J[i] for i in range(2))
+    problem.add_constraint(pc.sum(P) == 1)
+    problem.add_constraint(Y >> 0)
+    problem.add_constraint(Y0 == a * np.eye(dimA))
+    problem.add_list_of_constraints(p >= 0 for p in P)
+
+    problem.set_objective("min", a)
+
+    problem.solve(solver=solver, **kwargs)
+
+    return problem.value
+
+
+def _minimax_primal(
+    phi: np.ndarray | list[np.ndarray] | list[list[np.ndarray]],
+    psi: np.ndarray | list[np.ndarray] | list[list[np.ndarray]],
+    dimA: int,
+    dimB: int,
+    primal_dual="primal",
+    solver: str = "cvxopt",
+    **kwargs,
+) -> float:
+    """Find the primal problem for minimax quantum channel distinguishability SDP."""
+    J = list([phi, psi])
+
+    problem = pc.Problem()
+
+    a = pc.RealVariable("a", lower=0)
+    P = [pc.HermitianVariable(f"P[{i}]", (dimA * dimB, dimA * dimB)) for i in range(2)]
+    rho = pc.HermitianVariable("rho", (dimA, dimA))
+
+    problem.add_list_of_constraints(P[i] >> 0 for i in range(2))
+    problem.add_list_of_constraints(a <= pc.trace(P[i] * J[i]).real for i in range(2))
+    problem.add_constraint(pc.sum(P) == rho @ np.eye(dimB))
+    problem.add_constraint(rho >> 0)
+    problem.add_constraint(pc.trace(rho) == 1)
+
+    problem.set_objective("max", a)
+
+    problem.solve(solver=solver, **kwargs)
+
+    return problem.value
