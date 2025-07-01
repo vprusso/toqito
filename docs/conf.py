9fdbd15d2739c87ff2dec6383f196808d83de142
Wilson123d <140762786+Wilson123d@users.noreply.github.com>
2025-07-01 22:39:25 +0800

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,7 @@ import datetime
 
 # sys.path.insert(0, os.path.abspath("."))
 # sys.path.insert(0, os.path.abspath(".."))
-  sys.path.insert(0, os.path.abspath("../.."))
+sys.path.insert(0, os.path.abspath("../.."))
 
 # -- Project information -----------------------------------------------------
 
