895d5da6b526a27768292a220b703c37754e0775
RajdeepAher <aher_rp@ma.iitr.ac.in>
2025-08-20 19:29:18 +0530

    feat: add tests

--- toqito/perms/tests/test_swap.py
+++ toqito/perms/tests/test_swap.py
@@ -506,6 +506,13 @@ def test_swap(input_matrix, sys, dim, row_only, expected_result):
             None,
             False,
         ),
+        # Invalid sys parameters but their len(sys) == 2.
+        (
+            np.array([[1, 5, 9, 13], [2, 6, 10, 14], [3, 7, 11, 15], [4, 8, 12, 16]]),
+            [0, 20],
+            None,
+            False,
+        ),
         # Invalid sys parameters (length).
         (
             np.array([[1, 5, 9, 13], [2, 6, 10, 14], [3, 7, 11, 15], [4, 8, 12, 16]]),
