7ed1d1a3d5e69dc267b2c9632328b4408a8c29fb
Anna Almazán <84304910+Akri-A@users.noreply.github.com>
2026-02-24 14:24:40 +0100

    Delete toqito_landing_page directory

--- toqito_landing_page/themes/PaperMod/assets/css/core/license.css
+++
@@ -1,6 +0,0 @@
-/*
-  PaperMod v8+
-  License: MIT https://github.com/adityatelange/hugo-PaperMod/blob/master/LICENSE
-  Copyright (c) 2020 nanxiaobei and adityatelange
-  Copyright (c) 2021-2026 adityatelange
-*/
