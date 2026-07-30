6f74fe632913fcf3ede935beb1008d6587775243
Guan Tong <tguan@gmail.com>
2026-07-31 02:38:40 +0800

    docs: add runnable state examples

--- toqito/states/dicke.py
+++ toqito/states/dicke.py
@@ -13,15 +13,15 @@ def dicke(num_qubit: int, num_excited: int, return_dm: bool = False) -> np.ndarr
     distributed across the given number of qubits (i.e., `num_qubit`). It is symmetric and represents
     an equal superposition of all possible states with the specified number of excited qubits.
 
-    Example
-    Consider generating a Dicke state with 3 qubits and 1 excitation:
+    Examples:
+        Consider generating a Dicke state with 3 qubits and 1 excitation:
 
         ```python exec="1" source="above" result="text"
         from toqito.states import dicke
         print(dicke(3, 1))
         ```
 
-    If we request the density matrix for this state, the return value is:
+        If we request the density matrix for this state, the return value is:
 
         ```python exec="1" source="above" result="text"
         from toqito.states import dicke
